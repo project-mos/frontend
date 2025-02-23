73247788e0dad1d782a52fb834f8e0a055f257e9
강민재 <minijae011030@naver.com>
2025-02-23 16:51:18 +0900

    💄 Style: import 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Typography from "@/components/atoms/Typography";
-
 export default function Home() {
   return (
     <div className="flex h-200 items-center justify-center border-10 border-red-500 bg-white text-black"></div>
