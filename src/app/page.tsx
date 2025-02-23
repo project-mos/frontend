049c1a5bd66190412980d0d94131ac41611395aa
강민재 <minijae011030@naver.com>
2025-02-23 16:50:32 +0900

    🚀 [Feat]: Typography atom

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Typography from "@/components/atoms/Typography";
+
 export default function Home() {
   return (
     <div className="flex h-200 items-center justify-center border-10 border-red-500 bg-white text-black"></div>
