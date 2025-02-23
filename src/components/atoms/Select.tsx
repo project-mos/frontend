21e8a0e6feef3cb021c5dff44f8febda96de405e
gogleset <choij2494@gmail.com>
2025-02-23 19:04:49 +0900

    🚀 [Feat]: Atom/Select 컴포넌트 추가

---
+++ src/components/atoms/Select.tsx
@@ -0,0 +1,48 @@
+import { cn } from "@/lib/utils";
+import React, {
+  DetailedHTMLProps,
+  HtmlHTMLAttributes,
+  OptionHTMLAttributes,
+} from "react";
+
+interface SelectProps extends HtmlHTMLAttributes<HTMLSelectElement> {
+  className?: string;
+  placeholder?: string;
+}
+const Select = ({
+  className,
+  children,
+  placeholder,
+  ...props
+}: SelectProps) => {
+  return (
+    <select
+      className={cn(
+        "select select_renewal border rounded-md px-7 py-3 text-[24px] pr-[80px]",
+        className
+      )}
+      {...props}
+    >
+      {placeholder && (
+        <Option disabled hidden value="">
+          {placeholder}
+        </Option>
+      )}
+      {children}
+    </select>
+  );
+};
+
+const Option = ({
+  children,
+  ...props
+}: DetailedHTMLProps<
+  OptionHTMLAttributes<HTMLOptionElement>,
+  HTMLOptionElement
+>) => {
+  return <option {...props}>{children}</option>;
+};
+
+Select.Option = Option;
+
+export default Select;
