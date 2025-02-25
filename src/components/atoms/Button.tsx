a9825fa22c07bdc1388c4fa79047cc53a549b34a
gogleset <choij2494@gmail.com>
2025-02-25 15:52:01 +0900

    🚀 [Feat]: Atom/Button 컴포넌트 icon props 추가

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,18 +1,27 @@
 import { cn } from "@/lib/utils";
+import Image from "next/image";
 import React, { ButtonHTMLAttributes } from "react";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
   active?: boolean;
+  icon?: {
+    svg?: React.ReactNode; // SVG 요소 직접 전달 가능
+    src?: string; // 아이콘 파일 경로 (PNG, JPG, SVG 파일 가능)
+    alt?: string; // 아이콘 대체 텍스트
+  };
 }
 
 const DefaultButtonClass =
-  "px-5 py-2 border rounded-md border-gray-200 transition-all duration-200 text-sm bg-white text-mos-gray-700";
+  "px-5 py-2 border rounded-md border-gray-200 transition-all duration-200 text-sm bg-white text-mos-gray-700 flex items-center gap-2 h-[62px]";
+const SvgIconClass =
+  "w-6.5 h-6.5 fill-current text-current group-hover:text-current ";
 
 const DefaultButton: React.FC<ButtonProps> = ({
   className,
   children,
   active = false,
+  icon,
   ...props
 }) => {
   return (
@@ -21,7 +30,18 @@ const DefaultButton: React.FC<ButtonProps> = ({
       data-active={active}
       {...props}
     >
-      {children}
+      {/* SVG 직접 전달하는 경우 */}
+      {icon?.svg && <span className={cn(SvgIconClass)}>{icon.svg}</span>}
+
+      {/* 이미지 파일 경로로 전달하는 경우 Next.js 이미지 최적화 */}
+      {icon?.src && (
+        <Image width={26} height={26} src={icon.src} alt={icon.alt || "icon"} />
+      )}
+      {children && icon ? (
+        <div className={cn("ml-2")}>{children}</div>
+      ) : (
+        <>{children}</>
+      )}
     </button>
   );
 };
@@ -33,12 +53,14 @@ const MainButton: React.FC<ButtonProps> = ({
   className,
   children,
   active = false,
+  icon,
   ...props
 }) => {
   return (
     <DefaultButton
       className={cn(MainButtonClass, className)}
       active={active}
+      icon={icon}
       {...props}
     >
       {children}
@@ -53,12 +75,15 @@ const GrayButton: React.FC<ButtonProps> = ({
   className,
   children,
   active = false,
+  icon,
+
   ...props
 }) => {
   return (
     <DefaultButton
       className={cn(GrayButtonClass, className)}
       active={active}
+      icon={icon}
       {...props}
     >
       {children}
