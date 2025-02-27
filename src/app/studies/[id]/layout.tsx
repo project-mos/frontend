e076e3b57f02c7d3d75236c7375e151579149828
gogleset <choij2494@gmail.com>
2025-02-27 19:05:35 +0900

    💄 [Design]: layout grid gap 수정

--- src/app/studies/[id]/layout.tsx
+++ src/app/studies/[id]/layout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <Grid cols={12} gap={1}>
+    <Grid cols={12} gap={5}>
       {children}
     </Grid>
   );
