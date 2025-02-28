960645436a27e5d5b4e03da117c97ad98c0df1c5
gogleset <choij2494@gmail.com>
2025-02-28 11:40:53 +0900

    🚀 [Feat]: <StudyDescriptionCard/> 추가 🚀 [Feat]: MockStudiesApiResult 연동

--- src/app/studies/[id]/page.tsx
+++ src/app/studies/[id]/page.tsx
@@ -1,10 +1,12 @@
+import { MockStudiesApiResult } from "@/app/mock/api/studies";
 import Card from "@/components/atoms/Card";
+import StudyDescriptionCard from "@/features/studies/components/StudyDescriptionCard";
 import React from "react";
 
 const page = () => {
   return (
     <>
-      <Card className="col-span-8 min-h-screen"></Card>
+      <StudyDescriptionCard data={MockStudiesApiResult} />
       <Card className="col-span-4 flex flex-col h-20 "></Card>
     </>
   );
