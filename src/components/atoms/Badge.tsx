c7872a076a8e1969e841384798b0bae54a660abe
gogleset <choij2494@gmail.com>
2025-02-28 14:08:12 +0900

    🚀 [Feat]: Atom/Badge 컴포넌트 생성

---
+++ src/components/atoms/Badge.tsx
@@ -0,0 +1,36 @@
+import { cn } from "@/lib/utils";
+import React, { HTMLAttributes } from "react";
+
+const BadgeColors = {
+  Blue: "bg-mos-blue-500 text-white ",
+  Green: "bg-mos-green-500 text-white ",
+  Gray: "bg-mos-gray-500 text-white ",
+  Pink: "bg-mos-pink-500 text-white ",
+};
+
+interface BadgeProps extends HTMLAttributes<HTMLDivElement> {
+  className?: string;
+  color?: keyof typeof BadgeColors;
+}
+
+const Badge = ({
+  children,
+  color = "Blue",
+  className,
+  ...props
+}: BadgeProps) => {
+  return (
+    <span
+      className={cn(
+        "box-border flex items-center text-[.75rem] font-semibold px-2.5 rounded-full h-[20px]",
+        BadgeColors[color],
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </span>
+  );
+};
+
+export default Badge;
