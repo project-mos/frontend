bb96312d1f1c6a6504027cbb114cf3ba8dccd57d
yeooonn <rladuswjd0303@naver.com>
2025-03-05 21:45:34 +0900

    🚑️ [Fix]: Footer 반응형 대응 및 UI 개선

--- src/components/molecules/Footer.tsx
+++ src/components/molecules/Footer.tsx
@@ -26,39 +26,41 @@ interface FooterProps {
 }
 
 const Text: React.FC<FooterProps> = ({ children, className = "" }) => (
-  <Typography.P1 className={`text-mos-gray-300 ${className}`}>
+  <Typography.P1 className={`text-mos-gray-300 text-nowrap ${className}`}>
     {children}
   </Typography.P1>
 );
 
 const Footer = () => {
   return (
-    <div className="h-[365px] w-full bg-mos-white-gray-100 py-[56px] laptop:px-[100px] desktop:px-[300px]">
-      <div className="mb-[50px] flex gap-[300px]">
-        <div className="flex flex-col gap-2">
-          <SvgIcons.Logo />
-          <Text>함께 성장하는 스터디 플랫폼</Text>
-          <div className="flex gap-[15px]">
-            <i className="bi bi-github text-mos-gray-300"></i>
-            <i className="bi bi-instagram text-mos-gray-300"></i>
-            <i className="bi bi-facebook text-mos-gray-300"></i>
+    <div className="w-full h-auto bg-mos-white-gray-100 py-[56px]">
+      <div className="w-[90%] max-w-[1300px] mx-auto">
+        <div className="mb-[50px] flex flex-wrap justify-between gap-[50px]">
+          <div className="flex flex-col gap-2">
+            <SvgIcons.Logo />
+            <Text>함께 성장하는 스터디 플랫폼</Text>
+            <div className="flex gap-[15px]">
+              <i className="bi bi-github text-mos-gray-300"></i>
+              <i className="bi bi-instagram text-mos-gray-300"></i>
+              <i className="bi bi-facebook text-mos-gray-300"></i>
+            </div>
+          </div>
+          <div className="flex flex-wrap gap-[120px]">
+            {footerSections.map(({ title, list }) => (
+              <div key={title}>
+                <Text className="mb-[10px] font-bold">{title}</Text>
+                {list.map((list) => (
+                  <Text key={list}>{list}</Text>
+                ))}
+              </div>
+            ))}
           </div>
         </div>
-        <div className="flex gap-[130px]">
-          {footerSections.map(({ title, list }) => (
-            <div key={title}>
-              <Text className="mb-[10px] font-bold">{title}</Text>
-              {list.map((list) => (
-                <Text key={list}>{list}</Text>
-              ))}
-            </div>
-          ))}
+        <div className="w-full border-b border-gray-300"></div>
+        <div className="mt-[30px] flex flex-wrap justify-between gap-[20px]">
+          <Text>@2025 StudyMos. All rights reserved.</Text>
+          <Text>사업자등록번호: 123-45-67890 | 대표: 홍길동</Text>
         </div>
-      </div>
-      <div className="w-full border-b border-gray-300"></div>
-      <div className="mt-[30px] flex justify-between">
-        <Text>@2025 StudyMos. All rights reserved.</Text>
-        <Text>사업자등록번호: 123-45-67890 | 대표: 홍길동</Text>
       </div>
     </div>
   );
