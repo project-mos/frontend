ce675f3f1f655512dea43c5951271fdbef2cefa3
yeooonn <rladuswjd0303@naver.com>
2025-03-09 14:20:44 +0900

    🔨 [Refactor] : 마이페이지 컴포넌트 파일 위치 이동

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,7 +1,7 @@
-import Profile from "./components/Profile";
-import Statistics from "./components/Statistics";
-import StudySchedule from "./components/StudySchedule";
-import ActiveStudies from "./components/ActiveStudies";
+import ActiveStudies from "@/features/mypage/components/ActiveStudies";
+import Profile from "@/features/mypage/components/Profile";
+import Statistics from "@/features/mypage/components/Statistics";
+import StudySchedule from "@/features/mypage/components/StudySchedule";
 
 const MyPage = () => {
   return (
