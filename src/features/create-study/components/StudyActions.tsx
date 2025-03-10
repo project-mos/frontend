a26bc55a7902f87c45d8fc979b1f47489a8d9493
강민재 <minijae011030@naver.com>
2025-03-10 10:05:41 +0900

    🚑️ [Fix]: 취소버튼 버튼 타입 변경
    onSubmit 되지 않도록 함

--- src/features/create-study/components/StudyActions.tsx
+++ src/features/create-study/components/StudyActions.tsx
@@ -6,7 +6,7 @@ const StudyActions = () => {
       <Button.Solid color="Main" className="px-[40px]" active>
         다음 단계
       </Button.Solid>
-      <Button.Ghost color="Gray" className="px-[30px]">
+      <Button.Ghost type="button" color="Gray" className="px-[30px]">
         취소
       </Button.Ghost>
     </div>
