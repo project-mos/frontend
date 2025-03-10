2bd96cccdf158afdc2f1677dea17c800421072ea
Ian <105128049+minijae011030@users.noreply.github.com>
2025-03-10 17:35:57 +0900

    💄 [Design]: editor 디자인 수정

--- src/features/create-study/components/LabelEditor.tsx
+++ src/features/create-study/components/LabelEditor.tsx
@@ -28,10 +28,10 @@ const LabelEditor = ({ label, name, id, required }: LabelEditorProps) => {
       <Label htmlFor={id || name} label={label} required={required} />
       <div
         aria-labelledby="editor-label"
-        className="rounded-md border border-mos-gray-100"
+        className="h-[500px] overflow-hidden rounded-md border border-mos-gray-100"
       >
         <Editor
-          className="h-[500px]"
+          className="size-full"
           placeholder="스터디의 목표, 진행 방식, 기대 효과 등을 자세히 설명해주세요."
           markdown={content}
           onChange={handleChange}
