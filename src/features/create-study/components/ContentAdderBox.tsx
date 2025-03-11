1aa59f88f661bc0ef19c2ef47c7012c31e9f2bd6
Ian <105128049+minijae011030@users.noreply.github.com>
2025-03-11 09:50:16 +0900

    🚀 [Feat]: ContentAdderBox button 속성 추가

--- src/features/create-study/components/ContentAdderBox.tsx
+++ src/features/create-study/components/ContentAdderBox.tsx
@@ -2,19 +2,29 @@ import Button from "@/components/atoms/Button";
 import Card from "@/components/atoms/Card";
 import Typography from "@/components/atoms/Typography";
 
-interface ContentAdderBoxProps {
+interface ContentAdderBoxProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   buttonText: string;
   description: string;
 }
 
-const ContentAdderBox = ({ buttonText, description }: ContentAdderBoxProps) => {
+const ContentAdderBox = ({
+  buttonText,
+  description,
+  onClick,
+}: ContentAdderBoxProps) => {
   return (
     <Card>
       <Card.Content className="m-auto gap-[20px] py-[30px]">
         <Typography.P1 className="text-center text-[15px] text-mos-gray-700">
           {description}
         </Typography.P1>
-        <Button.Default active type="button" className="m-auto">
+        <Button.Default
+          onClick={onClick}
+          active
+          type="button"
+          className="m-auto"
+        >
           <i className="bi bi-plus"></i>
           {buttonText}
         </Button.Default>
