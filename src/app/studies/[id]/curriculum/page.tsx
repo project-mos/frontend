73b33c3c94d3cb7dcafbae6e1cac442ac9d46dcd
rladuswjd <jung@coontec.com>
2025-03-15 11:36:42 +0900

    ♻️ [Refactor]: 스터디룸 레이아웃 적용 및 기능 개선
    - 공통 컴포넌트에 스터디룸 레이아웃 적용
    - 커리큘럼 페이지 추가
    - DashboardSideBarCard에서 onClick 이벤트 추가 및 선택된 메뉴 활성화

---
+++ src/app/studies/[id]/curriculum/page.tsx
@@ -0,0 +1,5 @@
+const Curriculum = () => {
+  return <p>Curriculum</p>;
+};
+
+export default Curriculum;
--- src/app/studies/[id]/dashboard/page.tsx
+++ src/app/studies/[id]/dashboard/page.tsx
@@ -1,19 +1,8 @@
 import React from "react";
-import DashboardIntendedCard from "@/features/studies/components/dashboard/DashboardIntendedCard";
-import DashboardTitleCard from "@/features/studies/components/dashboard/DashboardTitleCard";
-import DashboardSideBarCard from "@/features/studies/components/dashboard/DashboardSideBarCard";
-
 import DashboardContentCard from "@/features/studies/components/dashboard/DashboardContentCard";
 
 const StudiesDashBoardPage = () => {
-  return (
-    <>
-      <DashboardTitleCard />
-      <DashboardIntendedCard />
-      <DashboardSideBarCard />
-      <DashboardContentCard />
-    </>
-  );
+  return <DashboardContentCard />;
 };
 
 export default StudiesDashBoardPage;
