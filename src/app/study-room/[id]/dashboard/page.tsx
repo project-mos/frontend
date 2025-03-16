c9cc73eb1da6e39d9f764278772ac2d11c774228
gogleset <choij2494@gmail.com>
2025-03-16 19:30:44 +0900

    🚚 [File]: study-room/[id]/dashboard 페이지 삭제

--- src/app/study-room/[id]/dashboard/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import DashboardContentCard from "@/features/study-room/components/dashboard/DashboardContentCard";
-
-const StudiesDashBoardPage = () => {
-  return <DashboardContentCard />;
-};
-
-export default StudiesDashBoardPage;
