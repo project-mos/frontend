75f9b32d68f360aaedec13e347a5e74a35c97cbc
gogleset <choij2494@gmail.com>
2025-03-16 20:18:21 +0900

    🚑️ [Fix]:DashboardContentCard 경로 수정

---
+++ src/app/studies/[id]/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import DashboardContentCard from "@/features/study-room/components/dashboard/DashboardContentCard";
+import React from "react";
+
+const StudiesDashBoardPage = () => {
+  return <DashboardContentCard />;
+};
+
+export default StudiesDashBoardPage;
