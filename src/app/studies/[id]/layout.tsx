56a7ca13c32d07a165460bdf69359b7296065df4
gogleset <choij2494@gmail.com>
2025-03-16 20:11:31 +0900

    🚑️ [Fix]: 페이지 layout 충돌 해결 -2

--- src/app/studies/[id]/layout.tsx
+++ src/app/studies/[id]/layout.tsx
@@ -1,15 +1,10 @@
 import Grid from "@/components/atoms/Grid";
-import DashboardIntendedCard from "@/features/studies/components/dashboard/DashboardIntendedCard";
-import DashboardSideBarCard from "@/features/studies/components/dashboard/DashboardSideBarCard";
-import DashboardTitleCard from "@/features/studies/components/dashboard/DashboardTitleCard";
+
 import React from "react";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <Grid cols={12} gap={5}>
-      <DashboardTitleCard />
-      <DashboardIntendedCard />
-      <DashboardSideBarCard />
       {children}
     </Grid>
   );
