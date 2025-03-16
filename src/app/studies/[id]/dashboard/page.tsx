9ad5d7df5a74f700a47a6f3eb24a39273d29332c
gogleset <choij2494@gmail.com>
2025-03-16 19:48:13 +0900

    Revert "🚚 [File]: studies/[id] 폴더 삭제"
    This reverts commit 94fc434a42290bdceb228945d5ae417e316a8ab2.

---
+++ src/app/studies/[id]/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import DashboardContentCard from "@/features/studies/components/dashboard/DashboardContentCard";
+
+const StudiesDashBoardPage = () => {
+  return <DashboardContentCard />;
+};
+
+export default StudiesDashBoardPage;
