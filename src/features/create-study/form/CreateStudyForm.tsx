6d24637ac947bc5e2bbffcb635e3fbd2072eea98
Ian <105128049+minijae011030@users.noreply.github.com>
2025-03-18 14:20:49 +0900

    🚀 [Feat]: 페이지 이동시 스터디 만들기 폼 초기화

--- src/features/create-study/form/CreateStudyForm.tsx
+++ src/features/create-study/form/CreateStudyForm.tsx
@@ -2,7 +2,7 @@
 import CreateStudyForm1 from "@/features/create-study/form/CreateStudyForm1";
 import CreateStudyForm2 from "@/features/create-study/form/CreateStudyForm2";
 import CreateStudyForm3 from "@/features/create-study/form/CreateStudyForm3";
-import { useSearchParams } from "next/navigation";
+import { usePathname, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 import { FormProvider, useForm } from "react-hook-form";
 import CreatStidyForm4 from "./CreateStudyForm4";
@@ -33,6 +33,7 @@ export interface StudyFormInterface {
 
 const CreateStudyForm = () => {
   const searchParams = useSearchParams();
+  const pathname = usePathname();
   const step = searchParams.get("step") || "1";
   const stepNumber = Number(step);
 
@@ -56,20 +57,29 @@ const CreateStudyForm = () => {
     },
   });
 
+  /** 폼 데이터 변경 시마다 localStorage에 저장 */
   useEffect(() => {
     const subscription = methods.watch((values) => {
       localStorage.setItem("studyForm", JSON.stringify(values));
     });
 
     return () => subscription.unsubscribe();
-  }, [methods.watch]);
+  }, [methods]);
 
+  /** 페이지 최초 로드 시 localStorage에서 불러오기 */
   useEffect(() => {
     const storedData = localStorage.getItem("studyForm");
     if (storedData) {
       methods.reset(JSON.parse(storedData));
     }
   }, []);
+
+  /** pathname이 변경되면 localStorage 초기화 */
+  useEffect(() => {
+    return () => {
+      localStorage.removeItem("studyForm");
+    };
+  }, [pathname]);
 
   return (
     <FormProvider {...methods}>
