9394774446f08c966424739c40592b28c85230b9
gogleset <choij2494@gmail.com>
2025-03-19 16:00:47 +0900

    🚀 [Feat]: LabelTextAreaInput 유효성 검사 기능 추가 (useFormContext)

--- src/features/create-study/components/LabelTextAreaInput.tsx
+++ src/features/create-study/components/LabelTextAreaInput.tsx
@@ -2,29 +2,51 @@ import React from "react";
 import { cn } from "@/lib/utils";
 import Label from "../../../components/molecules/Label";
 import Textarea from "@/components/atoms/Textarea";
+import {
+  FieldValues,
+  Path,
+  RegisterOptions,
+  useFormContext,
+} from "react-hook-form";
+import Typography from "@/components/atoms/Typography";
 
-interface LabelTextAreaInputProps
+interface LabelTextAreaInputProps<T extends FieldValues>
   extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
-  name: string;
+  name: Path<T>;
   id?: string;
+  placeholder: string;
+  registerOptions?: RegisterOptions<T, Path<T>>;
 }
-const LabelTextAreaInput = ({
+
+const LabelTextAreaInput = <T extends FieldValues>({
   label,
   name,
   className,
   required,
+  placeholder,
+  registerOptions,
   ...props
-}: LabelTextAreaInputProps) => {
+}: LabelTextAreaInputProps<T>) => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext<T>();
+
   return (
     <div className={cn("flex w-full flex-col gap-[5px]", className)} {...props}>
       <Label label={label} required={required} />
       <Textarea
-        name={name}
         className="h-[130px] resize-none"
-        placeholder="스터디 참여에 필요한 조건을 작성해주세요. (예: 필요한 기초 지식, 준비물, 참여 가능 시간 등)"
+        placeholder={placeholder}
         required={false}
+        {...register(name, registerOptions)}
       />
+      {errors[name]?.message && (
+        <Typography.P1 className="text-red-600">
+          {String(errors[name]?.message)}
+        </Typography.P1>
+      )}
     </div>
   );
 };
