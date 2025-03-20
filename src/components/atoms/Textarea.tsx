7d39cc5ca6818fa4e4bd14420da3b4c9933317d5
“yeooonn” <rladuswjd0303@naver.com>
2025-03-20 17:14:23 +0900

    ♻️ [Refactor]: useFormContext 제거

--- src/components/atoms/Textarea.tsx
+++ src/components/atoms/Textarea.tsx
@@ -1,5 +1,4 @@
 import { cn } from "@/lib/utils";
-import { useFormContext } from "react-hook-form";
 
 export interface TextareaProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
@@ -7,14 +6,11 @@ export interface TextareaProps
   name: string;
 }
 
-const Textarea = ({ className, name, ...props }: TextareaProps) => {
-  const { register } = useFormContext();
-
+const Textarea = ({ className, ...props }: TextareaProps) => {
   return (
     <textarea
-      {...register(name)}
       className={cn(
-        "h-[90px] w-full rounded-lg border border-mos-gray-100 p-[10px] text-[14px] text-mos-gray-700 placeholder:text-mos-gray-300 focus:border-mos-main-500 focus:outline-none focus:ring-mos-main-500",
+        "h-[90px] w-full rounded-lg border border-mos-gray-100 p-[10px] text-[14px] text-mos-gray-700 placeholder:text-mos-gray-500 focus:border-mos-main-500 focus:outline-none focus:ring-mos-main-500",
         className
       )}
       {...props}
