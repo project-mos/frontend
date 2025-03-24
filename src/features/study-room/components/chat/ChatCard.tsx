50d3aa7903810587f1b6716e8baf399a67d3e682
gogleset <choij2494@gmail.com>
2025-03-24 14:27:50 +0900

    💄 [Design]: ChatCard mobile 수정(min-height, input padding-right)

--- src/features/study-room/components/chat/ChatCard.tsx
+++ src/features/study-room/components/chat/ChatCard.tsx
@@ -11,7 +11,7 @@ const ChatCard = () => {
 
   return (
     <FormProvider {...methods}>
-      <Card className="col-span-12 gap-4 tablet:col-span-9 laptop:col-span-10">
+      <Card className="col-span-12 min-h-[500px] gap-4 tablet:col-span-9 laptop:col-span-10 ">
         <Card.Header className="flex items-center justify-between">
           <Typography.SubTitle1>채팅</Typography.SubTitle1>
           <Button.Solid color="Main" active size="sm">
@@ -23,8 +23,8 @@ const ChatCard = () => {
           <div className="bg-mos-white-gray-100"></div>
         </Card.Content>
         <Card.Footer className="relative">
-          <Input className="w-full px-3" />
-          <Button.Icon className="absolute right-0 h-full rounded-l-none border-mos-main bg-mos-main text-white">
+          <Input className="w-full pr-14" />
+          <Button.Icon className="absolute right-0 h-full rounded-l-none border-mos-main bg-mos-main px-4 text-white">
             <i className="bi bi-send"></i>
           </Button.Icon>
         </Card.Footer>
