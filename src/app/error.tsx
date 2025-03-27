f1471130b64b50d9cc82ef2e89b3b6bfbc6e18c3
gogleset <choij2494@gmail.com>
2025-03-27 12:43:32 +0900

    💄 [Design]: Error 페이지 스타일링 추가

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,5 +1,9 @@
 "use client"; // Error boundaries must be Client Components
 
+import Button from "@/components/atoms/Button";
+import Typography from "@/components/atoms/Typography";
+import URL from "@/constants/URL";
+import Link from "next/link";
 import { useEffect } from "react";
 
 export default function Error({
@@ -15,16 +19,31 @@ export default function Error({
   }, [error]);
 
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
-    </div>
+    <section className="flex h-full items-center p-16 dark:bg-gray-50 dark:text-gray-800">
+      <div className="container mx-auto my-8 flex flex-col items-center justify-center px-5">
+        <div className="flex max-w-md flex-col items-center justify-center gap-8 text-center">
+          <Typography.Head1 className="text-9xl font-extrabold dark:text-gray-400">
+            <span className="sr-only">Error</span>Error!
+          </Typography.Head1>
+          <Typography.Head3 className="md:text-3xl text-2xl font-semibold">
+            예기치 못한 오류입니다.
+          </Typography.Head3>
+          <div className="flex items-center justify-center gap-2">
+            <Link
+              rel="noopener noreferrer"
+              href={URL.HOME}
+              className="rounded font-semibold dark:bg-violet-600 dark:text-gray-50"
+            >
+              <Button.Solid color="Main" active>
+                홈페이지로 가기
+              </Button.Solid>
+            </Link>
+            <Button.Solid color="Red" active onClick={reset}>
+              다시 시도하기
+            </Button.Solid>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
