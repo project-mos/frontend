9562168b1083bac581e437bb0eab9dab37305363
gogleset <choij2494@gmail.com>
2025-03-29 17:05:58 +0900

    🚀 [Feat]: StudyRoomSessionCard 생성

---
+++ src/features/study-room/components/layout/StudyRoomSessionCard.tsx
@@ -0,0 +1,44 @@
+import Badge from "@/components/atoms/Badge";
+import Button from "@/components/atoms/Button";
+import Card from "@/components/atoms/Card";
+import Typography from "@/components/atoms/Typography";
+import React from "react";
+
+const StudyRoomSessionCard = ({ isEdit = false }: { isEdit?: boolean }) => {
+  return (
+    <Card className="col-span-12 gap-3 shadow-none mobile:gap-1 tablet:col-span-10">
+      <Card.Header className="justify-between">
+        <div className="flex flex-col gap-1 mobile:flex-row">
+          <Badge color="Blue">
+            <i className="bi bi-calendar3 mr-1"></i>
+            2024-02-25
+          </Badge>
+          <Badge color="Gray">
+            <i className="bi bi-clock mr-1"></i>
+            20:00 - 22:00
+          </Badge>
+        </div>
+        {isEdit && (
+          <div className="flex gap-2">
+            <Button.Icon color="Blue">
+              <i className="bi bi-pencil"></i>
+            </Button.Icon>
+            <Button.Icon color="Red">
+              <i className="bi bi-trash"></i>
+            </Button.Icon>
+          </div>
+        )}
+      </Card.Header>
+      <Card.Content>
+        <Typography.SubTitle1>알고리즘 기초 - 정렬</Typography.SubTitle1>
+      </Card.Content>
+      <Card.Footer>
+        <Typography.P3 className="text-mos-gray-300">
+          버블 정렬, 선택 정렬, 삽입 정렬에 대해 학습합니다.
+        </Typography.P3>
+      </Card.Footer>
+    </Card>
+  );
+};
+
+export default StudyRoomSessionCard;
