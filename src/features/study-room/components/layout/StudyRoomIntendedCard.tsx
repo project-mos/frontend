b2672350d5424c3ed91a6d1664c40e4490df7164
gogleset <choij2494@gmail.com>
2025-03-29 17:08:31 +0900

    🚀 [Feat]: StudyRoomSessionCard StudyRoomIntended, ScheduleCard에 추가

--- src/features/study-room/components/layout/StudyRoomIntendedCard.tsx
+++ src/features/study-room/components/layout/StudyRoomIntendedCard.tsx
@@ -2,21 +2,20 @@ import Button from "@/components/atoms/Button";
 import Card from "@/components/atoms/Card";
 import Typography from "@/components/atoms/Typography";
 import React from "react";
+import StudyRoomSessionCard from "./StudyRoomSessionCard";
 
 const StudyRoomIntendedCard = () => {
   return (
-    <Card className="col-span-12 tablet:col-span-4">
+    <Card className="col-span-12 gap-2 tablet:col-span-4">
       <Card.Header className="flex justify-between">
-        <Typography.SubTitle1>다음 스터디 일정</Typography.SubTitle1>
+        <Typography.SubTitle1>진행중인 스터디 일정</Typography.SubTitle1>
         <Button.Solid color="Green" active size="sm">
           <i className="bi bi-person-check text-[18px]"></i>
           출석하기
         </Button.Solid>
       </Card.Header>
       <Card.Content>
-        <Typography.P3 className="text-mos-gray-300">
-          예정된 일정이 없습니다
-        </Typography.P3>
+        <StudyRoomSessionCard />
       </Card.Content>
     </Card>
   );
--- src/features/study-room/components/schdule/ScheduleCard.tsx
+++ src/features/study-room/components/schdule/ScheduleCard.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import Badge from "@/components/atoms/Badge";
 import Button from "@/components/atoms/Button";
 import Card from "@/components/atoms/Card";
 import Typography from "@/components/atoms/Typography";
 
 import ScheduleModal from "./ScheduleModal";
 import useModal from "@/app/hooks/useModal";
+import StudyRoomSessionCard from "../layout/StudyRoomSessionCard";
 
 const ScheduleCard = () => {
   const { modal, openModal, closeModal } = useModal();
@@ -32,36 +32,7 @@ const ScheduleCard = () => {
           </div>
         </Card.Header>
         <Card.Content className="gap-3">
-          <Card className="col-span-12 gap-3 shadow-none mobile:gap-1 tablet:col-span-10">
-            <Card.Header className="justify-between">
-              <div className="flex flex-col gap-1 mobile:flex-row">
-                <Badge color="Blue">
-                  <i className="bi bi-calendar3 mr-1"></i>
-                  2024-02-25
-                </Badge>
-                <Badge color="Gray">
-                  <i className="bi bi-clock mr-1"></i>
-                  20:00 - 22:00
-                </Badge>
-              </div>
-              <div className="flex gap-2">
-                <Button.Icon color="Blue">
-                  <i className="bi bi-pencil"></i>
-                </Button.Icon>
-                <Button.Icon color="Red">
-                  <i className="bi bi-trash"></i>
-                </Button.Icon>
-              </div>
-            </Card.Header>
-            <Card.Content>
-              <Typography.SubTitle1>알고리즘 기초 - 정렬</Typography.SubTitle1>
-            </Card.Content>
-            <Card.Footer>
-              <Typography.P3 className="text-mos-gray-300">
-                버블 정렬, 선택 정렬, 삽입 정렬에 대해 학습합니다.
-              </Typography.P3>
-            </Card.Footer>
-          </Card>
+          <StudyRoomSessionCard isEdit />
         </Card.Content>
       </Card>
     </>
