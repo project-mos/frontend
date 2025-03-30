513ee7c677264a6ef784b916ceda8eb225c6c306
gogleset <choij2494@gmail.com>
2025-03-30 12:01:50 +0900

    💡 [Comment]: 진행중인 스터디 일정에 기획에 맞는 주석 추가

--- src/features/study-room/components/layout/StudyRoomIntendedCard.tsx
+++ src/features/study-room/components/layout/StudyRoomIntendedCard.tsx
@@ -6,6 +6,8 @@ import StudyRoomSessionCard from "./StudyRoomSessionCard";
 
 const StudyRoomIntendedCard = () => {
   return (
+    // 진행중인 일정같은 경우는 없다면 예정된 일정 중 가장 빠른걸로 보여준다.
+    // 미리 출석(스터디 시작하고 15분 후에는 지각이고, 스터디 시작 전 15분에는 미리출석가능)
     <Card className="col-span-12 gap-2 tablet:col-span-4">
       <Card.Header className="flex justify-between">
         <Typography.SubTitle1>진행중인 스터디 일정</Typography.SubTitle1>
