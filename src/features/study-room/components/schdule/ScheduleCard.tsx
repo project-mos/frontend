874af3ad166ca84e1d59f4413b08e2d68a8c7bb5
gogleset <choij2494@gmail.com>
2025-03-30 11:59:14 +0900

    🚀 [Feat]: ScheduleCard 안에서 중복 코드StudyScheduleCard로 컴포넌트화

--- src/features/study-room/components/schdule/ScheduleCard.tsx
+++ src/features/study-room/components/schdule/ScheduleCard.tsx
@@ -1,3 +1,4 @@
+ 
 "use client";
 
 import Button from "@/components/atoms/Button";
@@ -20,22 +21,48 @@ const ScheduleCard = () => {
           console.log("success!!");
         }}
       />
-
-      <Card className="col-span-12 h-fit gap-3 tablet:col-span-9 laptop:col-span-10">
-        <Card.Header className="flex-col justify-between gap-3">
-          <div className="flex justify-between">
-            <Typography.SubTitle1>스터디 일정</Typography.SubTitle1>
-            <Button.Solid color="Main" active size="sm" onClick={openModal}>
-              <i className="bi bi-plus text-xl" />
-              일정 추가
-            </Button.Solid>
-          </div>
-        </Card.Header>
-        <Card.Content className="gap-3">
-          <StudyRoomSessionCard isEdit />
-        </Card.Content>
-      </Card>
+      <div className="col-span-12 h-fit gap-3 tablet:col-span-9 laptop:col-span-10">
+        <StudyScheduleCard title="예정된 스터디 일정" onAdd={openModal}>
+          <StudyRoomSessionCard
+            onDelete={() => console.log("delete")}
+            onEdit={() => console.log("edit")}
+          />
+          <StudyRoomSessionCard />
+          <StudyRoomSessionCard />
+        </StudyScheduleCard>
+        <StudyScheduleCard title="마감된 스터디 일정">
+          <StudyRoomSessionCard />
+          <StudyRoomSessionCard />
+          <StudyRoomSessionCard />
+        </StudyScheduleCard>
+      </div>
     </>
+  );
+};
+
+const StudyScheduleCard = ({
+  title,
+  onAdd,
+  children,
+}: {
+  title: string;
+  onAdd?: () => void;
+  children: React.ReactNode;
+}) => {
+  return (
+    <Card className="mb-3 max-h-[400px] overflow-y-auto pt-0 tablet:max-h-[310px]">
+      <Card.Header className="sticky top-0 z-[1] flex flex-col justify-between gap-3 bg-white py-4">
+        <div className="flex w-full justify-between">
+          <Typography.SubTitle1>{title}</Typography.SubTitle1>
+          {onAdd && (
+            <Button.Solid color="Main" active size="sm" onClick={onAdd}>
+              <i className="bi bi-plus text-xl" /> 일정 추가
+            </Button.Solid>
+          )}
+        </div>
+      </Card.Header>
+      <Card.Content className="gap-3">{children}</Card.Content>
+    </Card>
   );
 };
 
