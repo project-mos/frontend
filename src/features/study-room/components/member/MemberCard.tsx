d2c593f7adc1fecf184c6142e30b5e897989ce75
gogleset <choij2494@gmail.com>
2025-03-31 14:22:15 +0900

    🚀 [Feat]:  MemberCard UI 개선(테이블 삭제)

--- src/features/study-room/components/member/MemberCard.tsx
+++ src/features/study-room/components/member/MemberCard.tsx
@@ -5,128 +5,206 @@ import Button from "@/components/atoms/Button";
 import Card from "@/components/atoms/Card";
 import Typography from "@/components/atoms/Typography";
 import { cn } from "@/lib/utils";
-import React from "react";
+import React, { useState } from "react";
 import MemberModal from "./MemberModal";
-import { MockStudyMembers } from "@/app/mock/api/study-room";
+import {
+  MockStudyMemberAttendance,
+  MockStudyMembers,
+} from "@/app/mock/api/study-room";
+import { StudyMemberInterface } from "@/types/api/study-room";
+// import {
+//   LineChart,
+//   Line,
+//   CartesianGrid,
+//   XAxis,
+//   YAxis,
+//   ResponsiveContainer,
+//   Tooltip,
+// } from "recharts";
 
-const thStyle = "px-1 py-2 text-[14px] tablet:text-sm";
+// import Profile from "@/components/atoms/Profile";
+// import profileImg from "../../../../app/asset/images/profile_example.jpeg";
 
 const MemberCard = () => {
   const { modal, openModal, closeModal } = useModal();
+  // 스터디원 조회
+  const [membersState] = useState(MockStudyMembers);
+  // 스터디원의 출석률 조회
+  const [memberAttendanceState] = useState(MockStudyMemberAttendance);
+  // 선택한 멤버
+  const [selectMemberAttendanceState, setSelectMemberAttendanceState] =
+    useState(() => {
+      if (memberAttendanceState.length > 0) {
+        return memberAttendanceState[0];
+      }
+    });
+
+  const onMoreHandler = (item: StudyMemberInterface) => {
+    // 멤버 출석율 조회에서 맞는 id 찾기
+    const find = memberAttendanceState.find(
+      (attendanceItem) => attendanceItem.userId === item.userId
+    );
+    if (find) {
+      setSelectMemberAttendanceState(find);
+    }
+    // modal 띄우기
+    openModal();
+  };
+
+  const onClose = () => {
+    // 선택한 멤버 초기화
+    setSelectMemberAttendanceState(undefined);
+    // modal 닫기
+    closeModal();
+  };
+
+  // 📌 날짜를 기반으로 몇 번째 주인지 계산하는 함수
+  // const getWeekNumber = (dateString: string): string => {
+  //   const date = new Date(dateString);
+  //   const firstDayOfYear = new Date(date.getFullYear(), 0, 1);
+  //   const pastDays = Math.floor(
+  //     (date.getTime() - firstDayOfYear.getTime()) / (1000 * 60 * 60 * 24)
+  //   );
+  //   return `W${Math.ceil((pastDays + firstDayOfYear.getDay() + 1) / 7)}`;
+  // };
+
+  // // 📌 주차별 평균 참여율 계산
+  // const getWeeklyAverageParticipation = (members: StudyMemberInterface[]) => {
+  //   const weeklyData: Record<string, number[]> = {};
+
+  //   members.forEach((member) => {
+  //     const week = getWeekNumber(member.lastAttendanceDate);
+  //     if (!weeklyData[week]) {
+  //       weeklyData[week] = [];
+  //     }
+  //     weeklyData[week].push(member.participationRate);
+  //   });
+
+  //   return Object.entries(weeklyData).map(([week, rates]) => ({
+  //     week,
+  //     averageParticipationRate:
+  //       rates.reduce((sum, rate) => sum + rate, 0) / rates.length,
+  //   }));
+  // };
+
+  // const weeklyChartData = getWeeklyAverageParticipation(membersState);
+  // console.log(weeklyChartData); // 결과 확인
   return (
     <>
       <MemberModal
         isOpen={modal}
-        onClose={closeModal}
-        memberName="홍길동"
-        email="hong@example.com"
-        experience="프론트엔드 2년차"
-        attendanceRate={90}
-        attendanceRecords={[
-          { date: "2024-03-25", status: "출석" },
-          { date: "2024-03-24", status: "지각" },
-          { date: "2024-03-23", status: "결석" },
-        ]}
+        data={selectMemberAttendanceState}
+        onClose={onClose}
       />
 
-      <Card className="h-fit gap-3 overflow-x-scroll">
+      <Card className="col-span-6 overflow-x-scroll">
+        <Card.Header>
+          <Typography.SubTitle1>전체 참여율</Typography.SubTitle1>
+        </Card.Header>
+        <Card.Content className="max-h-[450px] max-w-full flex-row gap-3">
+          {/* <ResponsiveContainer width="100%" height={180}>
+            <LineChart
+              data={weeklyChartData}
+              // margin={{ top: 20, right: 30, left: 10, bottom: 20 }}
+            >
+              <Line
+                type="monotone"
+                dataKey="averageParticipationRate"
+                stroke="#82ca9d"
+                strokeWidth={2}
+              />
+              <CartesianGrid stroke="#ccc" />
+              <XAxis
+                dataKey="week"
+                interval="preserveStartEnd"
+                padding={{ left: 0, right: 0 }}
+              />
+              <YAxis tickCount={5} domain={[10, 100]} width={30} />
+              <Tooltip />
+            </LineChart>
+          </ResponsiveContainer> */}
+        </Card.Content>
+      </Card>
+      <Card className="col-span-2 gap-2 overflow-x-scroll">
+        <Card.Header>
+          <Typography.SubTitle1>이 달의 우수 멤버</Typography.SubTitle1>
+        </Card.Header>
+        <Card.Content className="max-h-[450px]">
+          {membersState.length > 0 && (
+            <StudyMemberCard
+              data={membersState[0]}
+              // onMore={() => onMoreHandler(item)}
+            />
+          )}
+        </Card.Content>
+      </Card>
+      <Card className="col-span-8 h-fit gap-3 overflow-x-scroll">
         <Card.Header>
           <Typography.SubTitle1>멤버 관리</Typography.SubTitle1>
         </Card.Header>
-        <Card.Content className="max-h-[450px] gap-3 overflow-y-scroll ">
-          <table className="text-surface min-w-full text-left text-sm font-light">
-            <thead className="border-b border-neutral-200 font-medium ">
-              <tr>
-                <th scope="col" className={cn(thStyle)}>
-                  역할
-                </th>
-                <th scope="col" className={cn(thStyle)}>
-                  이름
-                </th>
-                <th scope="col" className={cn(thStyle)}>
-                  참여일
-                </th>
-                <th scope="col" className={cn(thStyle)}>
-                  참여율
-                </th>
-                <th scope="col" className={cn(thStyle)}>
-                  동작
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {MockStudyMembers.map((item, index) => {
-                return (
-                  <StudyMemberRow
-                    key={`${item}_${index}`}
-                    role={item.role}
-                    name={item.name}
-                    date={item.date}
-                    progress={item.progress}
-                    onChat={() => console.log("Chat clicked")}
-                    onMore={openModal}
-                  />
-                );
-              })}
-            </tbody>
-          </table>
+        <Card.Content className="max-h-[450px] max-w-full flex-row gap-3 overflow-x-scroll">
+          {membersState.map((item, index) => {
+            return (
+              <StudyMemberCard
+                key={`${item}_${index}`}
+                data={item}
+                onChat={() => console.log("chat")}
+                onMore={() => onMoreHandler(item)}
+              />
+            );
+          })}
         </Card.Content>
       </Card>
     </>
   );
 };
-
-const StudyMemberRow = ({
-  role,
-  name,
-  date,
-  progress,
+const StudyMemberCard = ({
+  data,
   onChat,
   onMore,
 }: {
-  role: "스터디장" | "스터디원";
-  name: string;
-  date: string;
-  progress: number;
-  onChat: () => void;
-  onMore: () => void;
+  data: StudyMemberInterface;
+  onChat?: () => void;
+  onMore?: () => void;
 }) => {
   return (
-    <tr className="border-b border-neutral-200">
-      <td className="w-fit p-2">
-        <Badge className="w-fit" color={role === "스터디장" ? "Green" : "Blue"}>
+    <Card className="min-w-52 gap-2 shadow-none">
+      <Card.Header className="flex-col items-center justify-center gap-2">
+        {/* <Profile width={80} height={80} src={profileImg} /> */}
+        <Typography.Head3>{data.nickname}</Typography.Head3>
+        <Badge
+          className="w-fit"
+          color={data.studyMemberRoleType === "스터디장" ? "Green" : "Blue"}
+        >
           <i
             className={cn(
               "bi text-[18px]",
-              role === "스터디장" ? "bi-person-gear" : "bi-person"
+              data.studyMemberRoleType === "스터디장"
+                ? "bi-person-gear"
+                : "bi-person"
             )}
           />
         </Badge>
-      </td>
-      <td className="p-2 font-medium">
-        <Typography.P3 className="text-[14px] tablet:text-sm">
-          {name}
-        </Typography.P3>
-      </td>
-      <td className="p-2">
         <Typography.P3 className="text-[14px] font-medium tablet:text-sm">
-          {date}
+          참여율 {data.participationRate}%
         </Typography.P3>
-      </td>
-      <td className="p-2">
-        <Typography.P3 className="text-[14px] font-medium tablet:text-sm">
-          {progress}%
+        <Typography.P3 className="text-[14px] font-medium text-mos-gray-300 tablet:text-[12px]">
+          최근 참여일 {data.lastAttendanceDate}
         </Typography.P3>
-      </td>
-      <td className="flex gap-2 p-2 ">
-        <Button.Icon color="Main" onClick={onChat}>
-          <i className="bi bi-chat"></i>
-        </Button.Icon>
-        <Button.Icon color="Main" onClick={onMore} className="flex">
-          <i className="bi bi-journal-check"></i>
-        </Button.Icon>
-      </td>
-    </tr>
+      </Card.Header>
+      <Card.Footer className="justify-center gap-2">
+        {onChat && (
+          <Button.Icon color="Main" onClick={onChat}>
+            <i className="bi bi-chat"></i>
+          </Button.Icon>
+        )}
+        {onMore && (
+          <Button.Icon color="Main" onClick={onMore} className="flex">
+            <i className="bi bi-journal-check"></i>
+          </Button.Icon>
+        )}
+      </Card.Footer>
+    </Card>
   );
 };
 
