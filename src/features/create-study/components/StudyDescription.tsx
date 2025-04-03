1048a45d9448a04d1019b5e2ff63379d93401e0e
Ian <105128049+minijae011030@users.noreply.github.com>
2025-04-03 13:48:28 +0900

    🚀 [Feat]: 스터디만들기 form2 버튼 활성화/비활성화 기능

--- src/features/create-study/components/StudyDescription.tsx
+++ src/features/create-study/components/StudyDescription.tsx
@@ -1,26 +1,16 @@
 import Card from "@/components/atoms/Card";
 import Typography from "@/components/atoms/Typography";
-import LabelEditor from "./LabelEditor";
 import LabelTextAreaInput from "../../../components/molecules/LabelTextAreaInput";
-import { QuillEditorHandle } from "@/components/QuillEditor";
+import LabelEditor from "./LabelEditor";
 
-interface StudyDescriptionProps {
-  editorRef: React.RefObject<QuillEditorHandle | null>;
-}
-
-const StudyDescription = ({ editorRef }: StudyDescriptionProps) => {
+const StudyDescription = () => {
   return (
     <Card>
       <Card.Header className="mb-[40px]">
         <Typography.SubTitle1>스터디 설명</Typography.SubTitle1>
       </Card.Header>
       <Card.Content className="flex flex-col gap-[25px]">
-        <LabelEditor
-          label="스터디 설명"
-          name="content"
-          editorRef={editorRef}
-          required={true}
-        />
+        <LabelEditor label="스터디 설명" name="content" required={true} />
         <LabelTextAreaInput
           label="참여 요건"
           name="requirements"
