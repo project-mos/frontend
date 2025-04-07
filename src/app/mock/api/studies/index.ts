168f7b05b1344b26093ea6e09cc8d9018bc2da03
gogleset <choij2494@gmail.com>
2025-04-07 14:12:44 +0900

    🚀 [Feat]: 내가 참여 중인 스터디의 일정 목록 조회 Type, Mock 데이터 추가

--- src/app/mock/api/studies/index.ts
+++ src/app/mock/api/studies/index.ts
@@ -2,6 +2,7 @@ import {
   StudiesDescription,
   StudyCardInterface,
   StudyCurriculumInterface,
+  StudyScheduleInterface,
 } from "@/types/api/studies/detail";
 
 // <StudyDescriptionCard/> 에 쓰일 mock data
@@ -60,23 +61,60 @@ export const MockStudyCardApiResult: StudyCardInterface = {
 };
 
 // <Curriculum/> 에 쓰일 mock data
-export const MockStudyCurriculumApiResult: StudyCurriculumInterface[] = [ 
+export const MockStudyCurriculumApiResult: StudyCurriculumInterface[] = [
   {
-    step: '1주차',
-    title: '정렬 알고리즘',
-    content: '버블정렬, 선택정렬, 삽입정렬, 퀵정렬학습',
-    task: ['백준 1181번','백준 1183번']
+    step: "1주차",
+    title: "정렬 알고리즘",
+    content: "버블정렬, 선택정렬, 삽입정렬, 퀵정렬학습",
+    task: ["백준 1181번", "백준 1183번"],
   },
   {
-    step: '2주차',
-    title: '그래프 탐색',
-    content: 'DFS, BFS 기초와 응용',
-    task: ['백준 1260번','백준 2178번']
+    step: "2주차",
+    title: "그래프 탐색",
+    content: "DFS, BFS 기초와 응용",
+    task: ["백준 1260번", "백준 2178번"],
   },
   {
-    step: '3주차',
-    title: '동적 프로그래밍',
-    content: 'DP 개념과 기초 문제 풀이',
-    task: ['백준 1463번','백준 9095번']
-  }
-]
+    step: "3주차",
+    title: "동적 프로그래밍",
+    content: "DP 개념과 기초 문제 풀이",
+    task: ["백준 1463번", "백준 9095번"],
+  },
+];
+
+// <StudyRoomSessionCard/>에 쓰일 mock data
+export const MockStudyScheduleApiResult: StudyScheduleInterface[] = [
+  {
+    studyScheduleId: 1,
+    title: "알고리즘 스터디",
+    description: "DP 문제 풀이",
+    startDateTime: "2025-03-20T10:00:00",
+    endDateTime: "2025-03-20T12:00:00",
+    studyId: 100,
+    studyCurriculumResList: [],
+  },
+  {
+    studyScheduleId: 2,
+    title: "CS 스터디",
+    description: "네트워크 기초",
+    startDateTime: "2025-03-22T14:00:00",
+    endDateTime: "2025-03-22T16:00:00",
+    studyId: 101,
+    studyCurriculumResList: [
+      {
+        studyCurriculumId: 20,
+        sectionId: 1,
+        title: "TCP/IP 개념",
+        content: "TCP와 UDP의 차이점 및 특징을 학습",
+      },
+      {
+        studyCurriculumId: 21,
+        sectionId: 2,
+        title: "HTTP 프로토콜",
+        content: "HTTP 요청과 응답 과정 분석",
+      },
+    ],
+  },
+];
+
+// 스터디 일정은 스터디 커리큘럼을 선택할 수 있기 때문에, 스터디 커리큘럼이 존재하는 스터디 일정은 studyCurriculumResList이 존재
