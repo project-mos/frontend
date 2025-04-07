c2b0196a171efa35ff7cae3a2206f4d39582b25d
gogleset <choij2494@gmail.com>
2025-04-07 14:15:45 +0900

    🚀 [Feat]: StudyRoomSessionCard Props 변경 data, handleEdit, handleDelete 추가

--- src/features/study-room/components/layout/StudyRoomSessionCard.tsx
+++ src/features/study-room/components/layout/StudyRoomSessionCard.tsx
@@ -1,15 +1,18 @@
 import Badge from "@/components/atoms/Badge";
-import Button from "@/components/atoms/Button";
+
 import Card from "@/components/atoms/Card";
 import Typography from "@/components/atoms/Typography";
+import { StudyScheduleInterface } from "@/types/api/studies/detail";
 import React from "react";
 
 const StudyRoomSessionCard = ({
-  onEdit,
-  onDelete,
+  handleEdit,
+  handleDelete,
+  data,
 }: {
-  onEdit?: () => void;
-  onDelete?: () => void;
+  data: StudyScheduleInterface;
+  handleEdit?: (data: number) => void;
+  handleDelete?: (data: number) => void;
 }) => {
   return (
     <Card className="col-span-12 gap-3 shadow-none mobile:gap-1 tablet:col-span-10">
@@ -17,33 +20,43 @@ const StudyRoomSessionCard = ({
         <div className="flex flex-col gap-1 mobile:flex-row">
           <Badge color="Blue">
             <i className="bi bi-calendar3 mr-1"></i>
-            2024-02-25
+            {data.startDateTime}
           </Badge>
           <Badge color="Gray">
             <i className="bi bi-clock mr-1"></i>
-            20:00 - 22:00
+            {data.endDateTime}
           </Badge>
         </div>
 
         <div className="z-1 absolute right-0 flex gap-2">
-          {onEdit && (
-            <Button.Icon color="Blue" onClick={onEdit}>
-              <i className="bi bi-pencil"></i>
-            </Button.Icon>
-          )}
-          {onDelete && (
-            <Button.Icon color="Red" onClick={onDelete}>
-              <i className="bi bi-trash"></i>
-            </Button.Icon>
-          )}
+          <div className="flex items-center gap-2">
+            {handleEdit && (
+              <i
+                className="bi bi-pencil-square cursor-pointer transition-all duration-200 hover:text-mos-main"
+                onClick={() => handleEdit(data.studyId)}
+              ></i>
+            )}
+            {handleDelete && (
+              <i
+                className="bi bi-trash cursor-pointer transition-all duration-200 hover:text-red-400"
+                onClick={() => handleDelete(data.studyId)}
+              ></i>
+            )}
+            {/* {handleCheckbox && (
+              <Checkbox
+                className="size-[15px] border border-mos-main"
+                onClick={() => handleCheckbox(data.studyId)}
+              />
+            )} */}
+          </div>
         </div>
       </Card.Header>
       <Card.Content>
-        <Typography.SubTitle1>알고리즘 기초 - 정렬</Typography.SubTitle1>
+        <Typography.SubTitle1>{data.title}</Typography.SubTitle1>
       </Card.Content>
       <Card.Footer>
         <Typography.P3 className="text-mos-gray-300">
-          버블 정렬, 선택 정렬, 삽입 정렬에 대해 학습합니다.
+          {data.description}
         </Typography.P3>
       </Card.Footer>
     </Card>
