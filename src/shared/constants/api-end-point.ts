c96b056045cfb2aea43f8e91aefb5c80d3086817
gogleset <choij2494@gmail.com>
2025-04-30 12:11:42 +0900

    🚀 [Feat]: api-end-point studies(get) 추가

--- src/shared/constants/api-end-point.ts
+++ src/shared/constants/api-end-point.ts
@@ -1,3 +1,4 @@
+import { GetStudiesRequest } from "@/shared/types/api/studies";
 import { Method } from "../utils/fetcher";
 
 export const API_ENDPOINT = {
@@ -5,7 +6,7 @@ export const API_ENDPOINT = {
     // 로그인
     signIn: () => {
       return {
-        url:`${process.env.MOS_API_BASE_URL}/oauth2/login`, // 임시 URL
+        url: `${process.env.MOS_API_BASE_URL}/oauth2/login`, // 임시 URL
         method: Method.POST,
       };
     },
@@ -21,18 +22,55 @@ export const API_ENDPOINT = {
     // 유저 정보 조회
     getUser: (id: number) => {
       return {
-        url:`${process.env.MOS_API_BASE_URL}/user/${id}`, // 임시 URL
+        url: `${process.env.MOS_API_BASE_URL}/user/${id}`, // 임시 URL
         method: Method.GET,
       };
     },
     // 유저 정보 수정
     updateUser: (id: number) => {
       return {
-        url:`${process.env.MOS_API_BASE_URL}/user/${id}`, // 임시 URL
+        url: `${process.env.MOS_API_BASE_URL}/user/${id}`, // 임시 URL
         method: Method.PATCH,
       };
-    }
-  }
+    },
+  },
+  studies: {
+    getStudies: ({
+      page = "1",
+      size = "12",
+      sort = "createAt,desc",
+      category,
+      meetType,
+      recruitmentStatus,
+      progressStatus,
+      liked,
+    }: GetStudiesRequest) => {
+      const params = new URLSearchParams({
+        page: page.toString(),
+        size: size.toString(),
+        sort,
+      });
 
+      if (category) params.append("category", category);
+      if (meetType) params.append("meetingType", meetType);
+      if (recruitmentStatus)
+        params.append("recruitmentStatus", recruitmentStatus);
+      if (progressStatus) params.append("progressStatus", progressStatus);
+      if (liked !== undefined) params.append("liked", liked.toString());
+
+      const url = `/studies?${params.toString()}`;
+      return {
+        url,
+        method: Method.GET,
+      };
+    },
+    getHotStudies: () => {
+      const url = "/studies/hots";
+      return {
+        url,
+        method: Method.GET,
+      };
+    },
+  },
   // 필요에 따라 추가
 };
