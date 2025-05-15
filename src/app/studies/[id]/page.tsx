8c92bc504cd28de2422d5cb0a4085b120790a364
gogleset <choij2494@gmail.com>
2025-05-15 21:50:45 +0900

    🚀 [Feat]: studyDetail 페이지 API 연결 추가

--- src/app/studies/[id]/page.tsx
+++ src/app/studies/[id]/page.tsx
@@ -3,13 +3,34 @@ import StudyCurriculumCard from "@/app/studies/components/StudyCurriculumCard";
 import StudyDescriptionCard from "@/app/studies/components/StudyDescriptionCard";
 import StudyLeaderCard from "@/app/studies/components/StudyLeaderCard";
 import Apply from "@/app/studies/components/applyStudy/Apply";
+import {
+  getBenefits,
+  getRequirements,
+  getRules,
+  getStudy,
+} from "@/features/studies/services/studies.service";
 
-import { MockStudiesApiResult } from "@/shared/mock/api/studies";
+// import { MockStudiesApiResult } from "@/shared/mock/api/studies";
 
-const page = () => {
+interface StudyDetailPageProps {
+  params: { id: string };
+}
+
+const page = async ({ params }: StudyDetailPageProps) => {
+  const { id } = await params;
+  const studyDetailData = await getStudy(id);
+  const requirementsData = await getRequirements(id);
+  const rulesData = await getRules(id);
+  const benefitsData = await getBenefits(id);
+
   return (
     <>
-      <StudyDescriptionCard data={MockStudiesApiResult} />
+      <StudyDescriptionCard
+        studyDetailData={studyDetailData}
+        requirementsData={requirementsData}
+        rulesData={rulesData}
+        benefitsData={benefitsData}
+      />
       <div className="order-last col-span-12 flex flex-col gap-5 tablet:order-none tablet:col-span-4">
         <StudyLeaderCard />
         <ShareCard />
--- src/app/studies/components/StudyDescriptionCard.tsx
+++ src/app/studies/components/StudyDescriptionCard.tsx
@@ -4,43 +4,61 @@ import Card from "@/shared/components/atoms/Card";
 import Tag from "@/shared/components/atoms/Tag";
 import Typography from "@/shared/components/atoms/Typography";
 import Meta from "@/shared/components/molecules/Meta";
+import { MDXRemote } from "next-mdx-remote-client/rsc";
 
 import {
-  Description,
-  StudiesDescriptionApiResult,
-} from "@/shared/types/api/studies/detail";
+  GetStudyBenefits,
+  GetStudyDetail,
+  GetStudyRequirements,
+  GetStudyRules,
+} from "@/shared/types/api/studies";
 
-const StudyDescriptionCard = ({ data }: StudiesDescriptionApiResult) => {
+interface StudyDescriptionCardProps {
+  studyDetailData: GetStudyDetail;
+  requirementsData: GetStudyRequirements;
+  rulesData: GetStudyRules;
+  benefitsData: GetStudyBenefits;
+}
+
+const StudyDescriptionCard = ({
+  studyDetailData,
+  requirementsData,
+  rulesData,
+  benefitsData,
+}: StudyDescriptionCardProps) => {
+  console.log(benefitsData);
   return (
     <Card className="col-span-12 flex flex-col gap-5 pb-10 tablet:col-span-8">
       <Card.Header className="flex-col gap-3">
         <div className="flex justify-between">
-          <Tag.Green bold>{data.category}</Tag.Green>
+          <Tag.Green bold>{studyDetailData.category}</Tag.Green>
           <div className="flex gap-2">
-            <Tag.Blue bold>{data.mode}</Tag.Blue>
+            <Tag.Blue bold>{studyDetailData.meetingType}</Tag.Blue>
             <Tag.Green bold border={false}>
-              {data.status}
+              {studyDetailData.recruitmentStatus}
             </Tag.Green>
           </div>
         </div>
         <div className="flex justify-between">
-          <Typography.Head3>{data.title}</Typography.Head3>
+          <Typography.Head3>{studyDetailData.title}</Typography.Head3>
         </div>
         <div className="flex flex-col gap-1">
           <Meta icon="calendar">
-            {data.schedule.startDate} ~ {data.schedule.endDate}
+            {studyDetailData.recruitmentStartDate} ~{" "}
+            {studyDetailData.recruitmentEndDate}
           </Meta>
-          <Meta icon="clock">{data.schedule.weeklyMeeting}</Meta>
+          <Meta icon="clock">{studyDetailData.schedule}</Meta>
           <div className="flex gap-2">
             <Meta icon="person">
-              {data.participants.current}/{data.participants.max}
+              {studyDetailData.currentStudyMemberCount}/
+              {studyDetailData.maxStudyMemberCount}
             </Meta>
-            <Meta icon="eye">{data.views}</Meta>
+            <Meta icon="eye">{studyDetailData.viewCount}</Meta>
           </div>
         </div>
 
         <div className="flex gap-2">
-          {data.tags.map((item, index) => {
+          {studyDetailData.tags.map((item, index) => {
             return (
               <Tag.Card key={`${item}_${index}`} bold>
                 #{item}
@@ -52,29 +70,27 @@ const StudyDescriptionCard = ({ data }: StudiesDescriptionApiResult) => {
       <Card.Content className="gap-5">
         <ContentWrapper>
           <Typography.SubTitle1>스터디 소개</Typography.SubTitle1>
-          <Typography.P1 className="text-mos-gray-700">
-            {data.description.intro}
-          </Typography.P1>
+          <MDXRemote source={studyDetailData.content} />
         </ContentWrapper>
 
         <ContentWrapper>
           <Typography.SubTitle1>참여 요건</Typography.SubTitle1>
           <div>
-            {data.description.requirements.map((item, index) => {
+            {requirementsData.map((item, index) => {
               return (
                 <Typography.P1
-                  key={`${item}_${index}`}
+                  key={`${item.id}_${index}`}
                   className="text-[14px] text-mos-gray-700"
                 >
-                  {item}
+                  {item.content}
                 </Typography.P1>
               );
             })}
           </div>
         </ContentWrapper>
 
-        {ListContent("스터디 규칙", data.description.rules)}
-        {ListContent("스터디 혜택", data.description.benefits)}
+        {ListContent("스터디 규칙", rulesData)}
+        {ListContent("스터디 혜택", benefitsData)}
       </Card.Content>
     </Card>
   );
@@ -84,13 +100,17 @@ const ContentWrapper = ({ children }: { children: React.ReactNode }) => {
   return <div className="flex flex-col gap-3">{children}</div>;
 };
 
-const ListContent = (title: string, items: Description["rules"]) => {
+const ListContent = (
+  title: string,
+  items: GetStudyRules | GetStudyBenefits
+) => {
+  console.log(items);
   return (
     <ContentWrapper>
       <Typography.SubTitle1>{title}</Typography.SubTitle1>
       <ul className="study-detail text-mos-gray-700">
         {items.map((item, index) => {
-          return <li key={`${item}_${index}`}> {item}</li>;
+          return <li key={`${item.id}_${index}`}> {item.content}</li>;
         })}
       </ul>
     </ContentWrapper>
