038e54927d03a4bc081e530d9b4733d0f2feea3d
Ian <105128049+minijae011030@users.noreply.github.com>
2025-05-20 20:45:29 +0900

    🚑️ [Fix]: clientRedirect 수정
    충돌 수정함

--- src/app/redirect/components/ClientRedirect.tsx
+++ src/app/redirect/components/ClientRedirect.tsx
@@ -15,25 +15,36 @@ const ClientRedirect = () => {
   const { setLoggedIn } = useAuthStore();
 
   const login = async () => {
-    const result = await oAuthLogin({ code: code!, provider: state! });
+    if (!code || !state) {
+      // (임시) 에러 클래스 생성할지 논의
+      throw new Error(
+        "OAuth 로그인 실패: 'code' 또는 'state'가 누락되었습니다."
+      );
+    }
+    const decodeURI = decodeURIComponent(state).split(",");
+    const provider = decodeURI[0];
+    const callbackUrl = decodeURI[1];
 
+    const result = await oAuthLogin({ code: code, provider });
     if (result?.status === 200) {
       localStorage.setItem("login", "true");
       localStorage.setItem("wasLoggedIn", "true");
       setLoggedIn(true);
-      router.replace(URL.HOME);
+
+      // 전역 관리된 Path가 있다면 그 URL로 리다이렉트 없다면 HOME으로 리다이렉트
+      router.replace(callbackUrl || URL.HOME);
       return;
     }
 
     localStorage.setItem("login", "false");
     setLoggedIn(false);
-    router.replace(URL.HOME);
+    return router.replace(URL.HOME);
   };
 
   useEffect(() => {
-    if (!code || !state) return;
     login();
   }, []);
+
   return <div></div>;
 };
 
