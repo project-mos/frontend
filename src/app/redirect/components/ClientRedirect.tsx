581f04315f276938d37c86eea7736285f0b237b3
gogleset <choij2494@gmail.com>
2025-05-20 14:32:30 +0900

    🚀 [Feat]: oAuth 리다이렉트 로직 중앙화(useAuthStore 연동)

--- src/app/redirect/components/ClientRedirect.tsx
+++ src/app/redirect/components/ClientRedirect.tsx
@@ -1,38 +1,47 @@
 "use client";
-import { useAuthStore } from "@/shared/store/authStore";
-import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect } from "react";
 
-import oAuthLogin from "@/features/login/services/oAuthLogin.service";
+import { useCallback, useEffect } from "react";
+
 import URL from "@/shared/constants/URL";
-
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
+import oAuthLogin from "@/features/login/services/oAuthLogin.service";
+import { useAuthStore } from "@/shared/store/authStore";
+//localhost:3000/redirect?code=dL-Gh6xbiBontb-h7akwffCUvKFf9_JHRnI3UWb7pbR8Pd3cD1oyTwAAAAQKFxZiAAABluupCZCGtS2__sNdBQ&state=KAKAO
 const ClientRedirect = () => {
   const searchParams = useSearchParams();
   const router = useRouter();
+
   const code = searchParams.get("code");
   const state = searchParams.get("state");
 
-  const { setLoggedIn } = useAuthStore();
+  const { setLoggedIn, redirectPath, hasHydrated } = useAuthStore();
 
-  const login = async () => {
-    const result = await oAuthLogin({ code: code!, provider: state! });
+  const memoLogin = useCallback(async () => {
+    if (!code || !state) {
+      // (임시) 에러 클래스 생성할지 논의
+      throw new Error(
+        "OAuth 로그인 실패: 'code' 또는 'state'가 누락되었습니다."
+      );
+    }
+    const result = await oAuthLogin({ code: code, provider: state });
 
     if (result?.status === 200) {
-      localStorage.setItem("login", "true");
       setLoggedIn(true);
-      router.replace(URL.HOME);
+      console.log(redirectPath);
+      // 전역 관리된 Path가 있다면 그 URL로 리다이렉트 없다면 HOME으로 리다이렉트
+      router.replace(redirectPath || URL.HOME);
       return;
     }
-
-    localStorage.setItem("login", "false");
     setLoggedIn(false);
-    router.replace(URL.HOME);
-  };
+    return router.replace(URL.HOME);
+  }, [code, state, setLoggedIn, router, redirectPath]);
 
   useEffect(() => {
-    if (!code || !state) return;
-    login();
-  }, []);
+    if (hasHydrated) {
+      memoLogin();
+    }
+  }, [hasHydrated, memoLogin]);
   return <div></div>;
 };
 
