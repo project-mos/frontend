183a35db3f706712dcb966a1f0d7fd1b0ef543cb
gogleset <choij2494@gmail.com>
2025-05-25 19:23:31 +0900

    🎨 [Style]: studies gap 조정

--- src/app/studies/[id]/layout.tsx
+++ src/app/studies/[id]/layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <section className="flex flex-col items-center gap-2">{children}</section>
+    <section className="flex flex-col items-center gap-7">{children}</section>
   );
 };
 
