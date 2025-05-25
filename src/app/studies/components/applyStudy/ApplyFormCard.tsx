cf46719505e7b24286b3147e0c1108efe7a4dc71
gogleset <choij2494@gmail.com>
2025-05-25 19:18:05 +0900

    🚀 [Feat]: width 비율 수정 및 require api 동기화

--- src/app/studies/components/applyStudy/ApplyFormCard.tsx
+++ src/app/studies/components/applyStudy/ApplyFormCard.tsx
@@ -5,41 +5,92 @@ import Button from "@/shared/components/atoms/Button";
 import Card from "@/shared/components/atoms/Card";
 import Typography from "@/shared/components/atoms/Typography";
 import LabelTextAreaInput from "@/shared/components/molecules/LabelTextAreaInput";
+import {
+  GetStudyQuestionsResponse,
+  PostStudyJoin,
+} from "@/shared/types/api/studies";
+
+import LabelRadioInput from "@/shared/components/molecules/LabelRadioInput";
+import { postJoin } from "@/features/studies/services/studies.service";
 
 interface ApplyFormCardInterface {
+  studyId: string;
+  data: GetStudyQuestionsResponse;
   setIsApplyVisible: Dispatch<SetStateAction<boolean>>;
 }
 
-interface formDataInterface {
-  experience: string;
-  goal: string;
-  motivation: string;
-}
-
-const ApplyFormCard = ({ setIsApplyVisible }: ApplyFormCardInterface) => {
-  const methods = useForm<formDataInterface>({
-    defaultValues: { motivation: "", experience: "", goal: "" },
+const ApplyFormCard = ({
+  studyId,
+  data,
+  setIsApplyVisible,
+}: ApplyFormCardInterface) => {
+  const methods = useForm({
     mode: "onChange",
   });
 
   const { handleSubmit, formState } = methods;
 
-  const onSubmit = (data: formDataInterface) => {
-    console.log("form data", data);
+  const onSubmit = async (data: Record<string, string>) => {
+    const transformedPostAPI: PostStudyJoin = Object.entries(data).map(
+      ([key, value]) => ({
+        studyQuestionId: Number(key),
+        answer: value,
+      })
+    );
+    try {
+      const response = await postJoin(studyId, transformedPostAPI);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
+  console.log(data, formState.isValid);
+
   return (
-    <Card className="h-auto w-full">
+    <Card className="h-auto w-[85%] ">
       <Card.Header className="mb-[15px]">
         <Typography.SubTitle1>지원양식</Typography.SubTitle1>
       </Card.Header>
 
       <Card.Content>
         <FormProvider {...methods}>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            {TextareaField("스터디를 지원하게 된 동기", "motivation")}
-            {TextareaField("관련 경험 및 보유 기술", "experience")}
-            {TextareaField("스터디를 통해 이루고 싶은 목표", "goal")}
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col gap-3"
+          >
+            {data.length > 0 ? (
+              data.map((item, index) => {
+                if (item.type === "주관식") {
+                  return TextareaField(
+                    item.question,
+                    String(item.id),
+                    item.required
+                  );
+                } else {
+                  const options = item.options.map((options) => {
+                    return { label: options, value: options };
+                  });
+                  return (
+                    <LabelRadioInput
+                      key={`${item}_${index}`}
+                      name={String(item.id)}
+                      label={item.question}
+                      options={options}
+                      required={item.required}
+                      registerOptions={getRegisterOptions(item.required)}
+                    />
+                  );
+                }
+              })
+            ) : (
+              <div className="flex justify-center p-10">
+                <Typography.SubTitle1 className="text-center text-mos-gray-500">
+                  이 모집은 별도의 지원 양식을 사용하지 않습니다.
+                  <br /> 자유롭게 지원해주세요!
+                </Typography.SubTitle1>
+              </div>
+            )}
 
             <div className="flex justify-center gap-[10px]">
               <Button.Ghost
@@ -67,16 +118,20 @@ const ApplyFormCard = ({ setIsApplyVisible }: ApplyFormCardInterface) => {
     </Card>
   );
 };
-
-const TextareaField = (label: string, name: string) => {
+const getRegisterOptions = (required: boolean) => {
+  return {
+    required: required ? "필수 입력 항목입니다." : false,
+  };
+};
+const TextareaField = (label: string, name: string, required: boolean) => {
   return (
-    <div className="mb-[10px]">
+    <div className="mb-[10px]" key={`${label}_${name}`}>
       <LabelTextAreaInput
         label={label}
         name={name}
         placeholder="내용을 입력해 주세요."
-        required
-        registerOptions={{ required: "필수 입력입니다." }}
+        required={required}
+        registerOptions={getRegisterOptions(required)}
       />
     </div>
   );
