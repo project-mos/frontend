8e819a15cbdf337436f08299e8ead44070ccb1a7
Ian <105128049+minijae011030@users.noreply.github.com>
2025-05-27 13:46:17 +0900

    🚀 [Feat]: 저장, 취소 모달 생성

--- src/app/(auth)/study-room/components/manage-overview/ContentInputBox.tsx
+++ src/app/(auth)/study-room/components/manage-overview/ContentInputBox.tsx
@@ -5,6 +5,9 @@ import React from "react";
 import Button from "@/shared/components/atoms/Button";
 import Input from "@/shared/components/atoms/Input";
 
+import ActionConfirmModal from "@/shared/components/molecules/ActionConfirmModal";
+import useMultiModal from "@/shared/hooks/useMultiModal";
+
 interface ContentInputBoxProps {
   value: string[];
   setValue: React.Dispatch<React.SetStateAction<string[]>>;
@@ -53,6 +56,8 @@ const ContentInputBox = ({
   placeholder,
   type,
 }: ContentInputBoxProps) => {
+  const { modal, openModal, closeModal } = useMultiModal();
+
   const handleAddValue = () => {
     setValue([...value, ""]);
   };
@@ -68,58 +73,85 @@ const ContentInputBox = ({
     setValue(newValue);
   };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const handleSubmit = () => {
     const arr =
       type === "rule"
         ? value.map((content, idx) => ({ content, ruleNum: idx + 1 }))
         : value.map((content, idx) => ({ content, benefitNum: idx + 1 }));
 
     console.log(arr);
+    closeModal("save");
     setState(false);
   };
 
   const handleCancelButton = () => {
+    closeModal("cancel");
     setState(false);
   };
 
   return (
-    <form onSubmit={handleSubmit} className="mt-4 px-4">
-      <div className="flex flex-col gap-3">
-        {value.map((data, index) => (
-          <InlineInput
-            key={data}
-            value={data}
-            onChange={(e) => handleEditValue(index, e.target.value)}
-            onRemove={() => handleDeleteValue(index)}
-            placeholder={placeholder}
-          />
-        ))}
-      </div>
-      <div className="flex w-full justify-between gap-2 py-4">
-        <Button.Default
-          type="button"
-          className="h-[35px]"
-          onClick={handleAddValue}
-        >
-          <i className="bi bi-plus" />
-          {buttonText}
-        </Button.Default>
-        <div className="flex gap-4">
-          <Button.Solid
-            color="Main"
-            type="button"
-            onClick={handleCancelButton}
-            className="h-[35px]"
-          >
-            취소하기
-          </Button.Solid>
-          <Button.Solid active color="Main" type="submit" className="h-[35px]">
-            저장하기
-          </Button.Solid>
+    <>
+      <form className="mt-4 px-4">
+        <div className="flex flex-col gap-3">
+          {value.map((data, index) => (
+            <InlineInput
+              key={data}
+              value={data}
+              onChange={(e) => handleEditValue(index, e.target.value)}
+              onRemove={() => handleDeleteValue(index)}
+              placeholder={placeholder}
+            />
+          ))}
         </div>
-      </div>
-    </form>
+        <div className="flex w-full justify-between gap-2 py-4">
+          <Button.Default
+            type="button"
+            className="h-[35px]"
+            onClick={handleAddValue}
+          >
+            <i className="bi bi-plus" />
+            {buttonText}
+          </Button.Default>
+          <div className="flex gap-4">
+            <Button.Solid
+              color="Main"
+              type="button"
+              onClick={() => openModal("cancel")}
+              className="h-[35px]"
+            >
+              취소하기
+            </Button.Solid>
+            <Button.Solid
+              active
+              color="Main"
+              type="button"
+              onClick={() => openModal("save")}
+              className="h-[35px]"
+            >
+              저장하기
+            </Button.Solid>
+          </div>
+        </div>
+      </form>
+      <ActionConfirmModal
+        isOpen={modal.get("cancel")!}
+        onClose={() => closeModal("cancel")}
+        onSuccess={handleCancelButton}
+        type="danger"
+        content="저장되지 않습니다. 취소하시겠습니까?"
+        title="취소하기"
+        buttonLabel="예, 취소합니다"
+      />
+      <ActionConfirmModal
+        isOpen={modal.get("save")!}
+        onClose={() => closeModal("save")}
+        onSuccess={handleSubmit}
+        type="action"
+        content="저장하시겠습니까?"
+        title="저장하기"
+        buttonLabel="저장하기"
+      />
+    </>
   );
 };
 
