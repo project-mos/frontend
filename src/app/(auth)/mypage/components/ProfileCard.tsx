f2b69e941ac95d6c74572f14bae2fc4d8a9e8033
yeooonn <rladuswjd0303@naver.com>
2025-05-28 02:28:25 +0900

    🚀 [Feat]: 프로필 데이터 초기값 추가

--- src/app/(auth)/mypage/components/ProfileCard.tsx
+++ src/app/(auth)/mypage/components/ProfileCard.tsx
@@ -22,8 +22,8 @@ const ProfileCard = ({ accessToken }: ProfileCardInterface) => {
   const { data: userInfo } = useQuery(userInfoQueryOption(accessToken!));
 
   const {
-    nickname,
-    introduction,
+    nickname = "이름",
+    introduction = "한 줄 소개를 등록해 주세요.",
     // categories,
     profileImage = profileImg,
     // joinDate = "0000-00-00",
@@ -41,7 +41,7 @@ const ProfileCard = ({ accessToken }: ProfileCardInterface) => {
           />
           <Typography.Head3>{nickname}</Typography.Head3>
           <Typography.P3 className="mb-[26px] text-[14px] text-mos-gray-500">
-            {introduction ? introduction : "한 줄 소개를 등록해 주세요."}
+            {introduction}
           </Typography.P3>
           {/* <div className="mb-[10px] flex gap-2">
             {Array.isArray(categories)
