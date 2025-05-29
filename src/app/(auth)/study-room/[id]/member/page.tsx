8421be683344b957e475134b52e36f8762e31946
Ian <105128049+minijae011030@users.noreply.github.com>
2025-05-29 10:17:44 +0900

    ♻️ [Refactor]: studyDetailPageProps 인터페이스를 타입 파일로 분리해 export

--- src/app/(auth)/study-room/[id]/member/page.tsx
+++ src/app/(auth)/study-room/[id]/member/page.tsx
@@ -1,6 +1,9 @@
 import MemberCard from "@/app/(auth)/study-room/components/member/MemberCard";
+import { StudyDetailPageProps } from "@/features/study-room/types/study-room.type";
 
-const MemberPage = () => {
+const MemberPage = async ({ params }: StudyDetailPageProps) => {
+  const { id } = await params;
+  console.log(id);
   return (
     <div className="grid grid-cols-8 gap-5">
       <MemberCard />
