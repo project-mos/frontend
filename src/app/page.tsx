865e7c6a5e88fba15cc2fa3e5914a4003fab835e
gogleset <choij2494@gmail.com>
2025-06-05 09:45:37 +0900

    🚑️ [Fix]: GetCategories 컨벤션에 맞게 네이밍 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,9 @@ import LandingLoginToast from "@/features/landing/components/LandingLoginToast";
 import Pagination from "@/shared/components/molecules/Pagination";
 
 import LandingStudiesBoundary from "@/features/landing/components/LandingStudiesBoundary";
-import { GetCategories } from "@/features/landing/services/landing.service";
+
 import { GetStudiesRequest } from "@/features/landing/types/landing.api";
+import { getCategories } from "@/features/landing/services/landing.service";
 
 interface HomeProps {
   searchParams: Promise<GetStudiesRequest>;
@@ -14,7 +15,7 @@ interface HomeProps {
 export default async function Home({ searchParams }: HomeProps) {
   const studiesRequest = await searchParams;
   const { page: currentPage } = studiesRequest;
-  const categoriesData = await GetCategories();
+  const categoriesData = await getCategories();
 
   return (
     <div className="flex flex-col gap-10 ">
