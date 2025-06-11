49e632404e4e3bb33d2a87e80f482372e514ac23
gogleset <choij2494@gmail.com>
2025-06-11 10:13:35 +0900

    🚀 [Feat]: 소개 페이지 스터디 소개 카드 추가

---
+++ src/app/(auth)/study-room/components/manage-overview/ManageContentCard.tsx
@@ -0,0 +1,25 @@
+import { GetStudyDetailResponse } from "@/features/studies/types/studies.api";
+import Card from "@/shared/components/atoms/Card";
+import Typography from "@/shared/components/atoms/Typography";
+import CustomMdxRemote from "@/shared/components/system/CustomMdxRemote";
+import React from "react";
+
+interface ContentCardProps {
+  data: GetStudyDetailResponse;
+}
+
+const ManageContentCard = ({ data }: ContentCardProps) => {
+  return (
+    <Card>
+      <Card.Header className="mb-[10px] justify-between border-b pb-2">
+        <Typography.SubTitle1>스터디 소개</Typography.SubTitle1>
+      </Card.Header>
+
+      <Card.Content className="prose prose-sm max-w-full">
+        <CustomMdxRemote content={data.content} />
+      </Card.Content>
+    </Card>
+  );
+};
+
+export default ManageContentCard;
