c210a288d06eb02f043820190511a1652192089f
gogleset <choij2494@gmail.com>
2025-06-14 18:05:30 +0900

    ♻️ [Refactor]: useDecodeToken 리팩토링 useMemo 삭제, useTokenStore 내재화

--- src/shared/hooks/useDecodeToken.tsx
+++ src/shared/hooks/useDecodeToken.tsx
@@ -1,19 +1,19 @@
 "use client";
-import { useMemo } from "react";
+import { useTokenStore } from "@/shared/store/authStore";
 
-const useDecodeToken = (accessToken?: string) => {
-  return useMemo(() => {
-    if (!accessToken) return null;
+const useDecodeToken = () => {
+  const { accessToken } = useTokenStore();
 
-    try {
-      const payloadBase64 = accessToken.split(".")[1];
-      const decodedPayload = JSON.parse(atob(payloadBase64));
-      return decodedPayload;
-    } catch (error) {
-      console.error("토큰 디코딩 실패:", error);
-      return null;
-    }
-  }, [accessToken]);
+  if (!accessToken) return null;
+
+  try {
+    const payloadBase64 = accessToken.split(".")[1];
+    const decodedPayload = JSON.parse(atob(payloadBase64));
+    return decodedPayload;
+  } catch (error) {
+    console.error("토큰 디코딩 실패:", error);
+    return null;
+  }
 };
 
 export default useDecodeToken;
