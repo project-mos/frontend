bcd0350b8ea2057fa2e78be5015afb4e5cd71243
gogleset <choij2494@gmail.com>
2025-06-15 21:25:39 +0900

    🎨 [Style]: Redirect 페이지 스타일 추가

--- src/app/redirect/page.tsx
+++ src/app/redirect/page.tsx
@@ -1,7 +1,35 @@
-import ClientRedirect from "./components/ClientRedirect";
+"use client";
+import { useEffect, useState } from "react";
+import Typography from "@/shared/components/atoms/Typography";
+import ClientRedirect from "@/app/redirect/components/ClientRedirect";
 
 const Redirect = () => {
-  return <ClientRedirect />;
+  const [dotCount, setDotCount] = useState(2); // 처음 ".." 부터 시작
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDotCount((prev) => (prev >= 5 ? 1 : prev + 1));
+    }, 500); // 0.5초마다
+
+    return () => clearInterval(interval); // cleanup
+  }, []);
+
+  return (
+    <section className="flex h-[600px] items-center p-16 dark:bg-gray-50 dark:text-gray-800">
+      <div className="container mx-auto my-8 flex flex-col items-center justify-center px-5">
+        <div className="flex max-w-md flex-col items-center justify-center gap-8 text-center">
+          <Typography.Head3 className="text-8xl font-extrabold dark:text-gray-400">
+            <span className="sr-only">Redirect</span>
+            이동중{".".repeat(dotCount)}
+          </Typography.Head3>
+          <Typography.Head3 className="md:text-3xl text-2xl font-semibold">
+            잠시만 기다려주세요!
+          </Typography.Head3>
+        </div>
+      </div>
+      <ClientRedirect />
+    </section>
+  );
 };
 
 export default Redirect;
