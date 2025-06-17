0b1e765b9fbfbed5ea393fa3faf48c32ed67a431
gogleset <choij2494@gmail.com>
2025-06-17 10:53:43 +0900

    🚑️ [Fix]: Query 키에 맞춰서 params 형변환

--- src/app/(auth)/study-room/components/layout/StudyRoomTitleCard.tsx
+++ src/app/(auth)/study-room/components/layout/StudyRoomTitleCard.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { GetStudyDetailResponse } from "@/features/studies/types/studies.api";
-import { useGetStudySchedule } from "@/features/study-room/hooks/useStudyRoomQueries";
+import { useGetStudySchedule } from "@/features/study-room/hooks/useScheduleQueries";
 
 import Card from "@/shared/components/atoms/Card";
 import Tag from "@/shared/components/atoms/Tag";
@@ -12,7 +12,7 @@ import { useParams } from "next/navigation";
 const StudyRoomTitleCard = ({ data }: { data: GetStudyDetailResponse }) => {
   const params = useParams() as { id: string };
 
-  const { data: scheduleData } = useGetStudySchedule(params.id);
+  const { data: scheduleData } = useGetStudySchedule(Number(params.id));
   const formatScheduleDate = getFormatStartEndDate();
 
   const getProgress = (): number => {
