a6d59f5e2f7661f9c0acbcf2c4974211498c6e67
gogleset <choij2494@gmail.com>
2025-06-17 10:54:45 +0900

    ♻️ [Refactor]: 진행중인 스터디 일정 (출석하기 기능 회의 후 결정, 1차 기능 완성)

--- src/app/(auth)/study-room/components/layout/StudyRoomIntendedCard.tsx
+++ src/app/(auth)/study-room/components/layout/StudyRoomIntendedCard.tsx
@@ -7,41 +7,47 @@ import Card from "@/shared/components/atoms/Card";
 import Typography from "@/shared/components/atoms/Typography";
 
 import { useParams } from "next/navigation";
-import { useGetStudySchedule } from "@/features/study-room/hooks/useStudyRoomQueries";
+import { useGetStudySchedule } from "@/features/study-room/hooks/useScheduleQueries";
 
 const StudyRoomIntendedCard = () => {
   const params = useParams() as { id: string };
 
-  const { data: scheduleData } = useGetStudySchedule(params.id);
-  const isOngoingStudy = getIsOngoingStudy();
+  const { data: scheduleData } = useGetStudySchedule(Number(params.id));
+  const now = Date.now();
+  const getIsStudyingTime = (startDateTime: string, endDateTime: string) => {
+    const start = new Date(startDateTime).getTime() - 15 * 60 * 1000;
+    const end = new Date(endDateTime).getTime();
+    return now >= start && now <= end;
+  };
+  const filteredSchedules = scheduleData?.filter((item) => {
+    return getIsStudyingTime(item.startDateTime, item.endDateTime);
+  });
 
-  function getIsOngoingStudy() {
-    if (scheduleData && scheduleData?.length > 0) {
-      const now = Date.now();
-      const start =
-        new Date(scheduleData[0].startDateTime).getTime() - 15 * 60 * 1000;
-      const end = new Date(scheduleData[0].endDateTime).getTime();
+  const isActive = filteredSchedules && filteredSchedules.length > 0;
 
-      return now >= start && now <= end;
-    } else {
-      return null;
-    }
-  }
-
+  // const buttonActive =
+  //   filteredSchedules &&
+  //   getIsStudyingTime(
+  //     filteredSchedules[0].startDateTime,
+  //     filteredSchedules[0].endDateTime
+  //   );
   return (
     // 진행중인 일정같은 경우는 없다면 예정된 일정 중 가장 빠른걸로 보여준다.
     // 미리 출석(스터디 시작하고 15분 후에는 지각이고, 스터디 시작 전 15분에는 미리출석가능)
     <Card className="col-span-12 gap-2 tablet:col-span-4">
       <Card.Header className="flex justify-between">
         <Typography.SubTitle1>진행중인 스터디 일정</Typography.SubTitle1>
-        <Button.Solid color="Green" active={isOngoingStudy || false} size="sm">
+        <Button.Solid color="Green" size="sm">
           <i className="bi bi-person-check text-[18px]"></i>
           출석하기
         </Button.Solid>
       </Card.Header>
       <Card.Content className="h-full items-center justify-center">
-        {isOngoingStudy ? (
-          scheduleData && <StudyRoomSessionCard data={scheduleData[0]} />
+        {isActive ? (
+          <StudyRoomSessionCard
+            data={filteredSchedules[0]}
+            className="w-full"
+          />
         ) : (
           <Typography.P3 className="text-mos-gray-500">
             진행중인 스터디가 없습니다!
