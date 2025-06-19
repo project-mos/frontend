9fd285be67f33481b46edb3418c15391830820cc
Ian <105128049+minijae011030@users.noreply.github.com>
2025-06-19 11:18:45 +0900

    🚀 [Feat]: 스터디 나가기 기능

--- src/app/(auth)/study-room/[id]/setting/page.tsx
+++ src/app/(auth)/study-room/[id]/setting/page.tsx
@@ -1,7 +1,10 @@
+import { StudyDetailPageProps } from "@/features/study-room/types/study-room.type";
 import SettingCard from "../../components/setting/SettingCard";
 
-const SettingPage = () => {
-  return <SettingCard />;
+const SettingPage = async ({ params }: StudyDetailPageProps) => {
+  const { id } = await params;
+
+  return <SettingCard studyId={id} />;
 };
 
 export default SettingPage;
