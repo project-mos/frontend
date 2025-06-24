ea6eb3f2daa37a3a6d9ec6c17a512ec50f28b479
gogleset <choij2494@gmail.com>
2025-06-24 15:11:19 +0900

    🚑️ [Fix]: study 안쓰는 컴포넌트 제거 및 통합

--- src/features/studies/components/StudyCard.tsx
+++ src/features/studies/components/StudyCard.tsx
@@ -6,6 +6,8 @@ import Typography from "@/shared/components/atoms/Typography";
 import Meta from "@/shared/components/molecules/Meta";
 import { Study } from "@/features/landing/types/landing.api";
 import CustomMdxRemote from "@/shared/components/system/CustomMdxRemote";
+import Link from "next/link";
+import URL from "@/shared/constants/URL";
 
 interface StudyCardProps extends HTMLAttributes<HTMLDivElement> {
   data: Study;
@@ -20,53 +22,58 @@ const StudyCard = ({ data, className, ...props }: StudyCardProps) => {
     });
   };
   return (
-    <Card
-      className={`flex h-[300px] w-[255px] cursor-pointer flex-col justify-between ${
-        className ?? ""
-      }`}
-      {...props}
+    <Link
+      href={`${URL.STUDY.DETAIL(data.id)}`}
+      className="flex justify-center "
     >
-      <Card.Header className="flex-col gap-2">
-        <div className="flex justify-between">
-          <div className="flex gap-[4.5px]">
-            <Tag.Green bold>{data.category}</Tag.Green>
-            <Tag.Blue bold>{data.meetingType}</Tag.Blue>
+      <Card
+        className={`flex h-[300px] w-[255px] cursor-pointer flex-col justify-between ${
+          className ?? ""
+        }`}
+        {...props}
+      >
+        <Card.Header className="flex-col gap-2">
+          <div className="flex justify-between">
+            <div className="flex gap-[4.5px]">
+              <Tag.Green bold>{data.category}</Tag.Green>
+              <Tag.Blue bold>{data.meetingType}</Tag.Blue>
+            </div>
+            {data.recruitmentStatus === "모집 중" ? (
+              <Tag.Green bold border={false}>
+                모집중
+              </Tag.Green>
+            ) : (
+              <Tag.Gray bold border={false}>
+                모집 완료
+              </Tag.Gray>
+            )}
           </div>
-          {data.recruitmentStatus === "모집 중" ? (
-            <Tag.Green bold border={false}>
-              모집중
-            </Tag.Green>
-          ) : (
-            <Tag.Gray bold border={false}>
-              모집 완료
-            </Tag.Gray>
-          )}
-        </div>
-        <Typography.Head3 className="truncate">{data.title}</Typography.Head3>
+          <Typography.Head3 className="truncate">{data.title}</Typography.Head3>
 
-        <div className="flex flex-col gap-1">
-          <Meta icon="calendar" className="text-sm text-black">
-            {data.recruitmentStartDate} ~ {data.recruitmentEndDate}
-          </Meta>
-        </div>
-      </Card.Header>
-      <Card.Content className="">
-        <div className="h-[50px] max-w-full truncate text-mos-gray-700">
-          <CustomMdxRemote content={data.content} preview />
-        </div>
-      </Card.Content>
-      <div className="flex flex-wrap gap-[5px]">{renderTags()}</div>
-      <Card.Footer className="flex justify-between">
-        <div className="flex gap-1  text-mos-gray-300">
-          <Meta icon="person">
-            {data.currentStudyMembers}/{data.maxStudyMembers}명
-          </Meta>
-        </div>
-        <div className="flex gap-1 text-mos-gray-300">
-          <Meta icon="eye">{data.viewCount}</Meta>
-        </div>
-      </Card.Footer>
-    </Card>
+          <div className="flex flex-col gap-1">
+            <Meta icon="calendar" className="text-sm text-black">
+              {data.recruitmentStartDate} ~ {data.recruitmentEndDate}
+            </Meta>
+          </div>
+        </Card.Header>
+        <Card.Content className="">
+          <div className="h-[50px] max-w-full truncate text-mos-gray-700">
+            <CustomMdxRemote content={data.content} preview />
+          </div>
+        </Card.Content>
+        <div className="flex flex-wrap gap-[5px]">{renderTags()}</div>
+        <Card.Footer className="flex justify-between">
+          <div className="flex gap-1  text-mos-gray-300">
+            <Meta icon="person">
+              {data.currentStudyMembers}/{data.maxStudyMembers}명
+            </Meta>
+          </div>
+          <div className="flex gap-1 text-mos-gray-300">
+            <Meta icon="eye">{data.viewCount}</Meta>
+          </div>
+        </Card.Footer>
+      </Card>
+    </Link>
   );
 };
 
