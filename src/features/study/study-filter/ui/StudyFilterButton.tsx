087331dd9eebe2524a17f6015d4b4c13f8612ca9
gogleset <choij2494@gmail.com>
2025-06-25 09:14:05 +0900

    🚀 [Feat]: useStudyFilter 추가 및 적용

--- src/features/study/study-filter/ui/StudyFilterButton.tsx
+++ src/features/study/study-filter/ui/StudyFilterButton.tsx
@@ -1,23 +1,21 @@
 "use client";
 
+import useStudyFilter from "@/features/study/study-filter/hook/useStudyFilter";
 import {
   StudyCategories,
   StudyFilterButtonProps,
 } from "@/features/study/study-filter/ui/study-filter.ui.types";
 import Button from "@/shared/components/atoms/Button";
 import cn from "@/shared/utils/cn";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
 import React from "react";
 
 const StudyFilterButton = ({
   categories,
   ...props
 }: StudyFilterButtonProps) => {
-  const currentSearchParams = useSearchParams();
-  const router = useRouter();
-  const pathname = usePathname();
-
   const { categories: categoriesData } = categories;
+  const { onClickCategory, searchParams } = useStudyFilter();
 
   // label, value 방식으로 변환(임시적용일 수 있음)
   const makeLabelCategoriesData: StudyCategories = categoriesData.map(
@@ -30,18 +28,6 @@ const StudyFilterButton = ({
     makeLabelCategoriesData.unshift({ label: "전체", value: null });
   }
 
-  const onClickCategory = (value: string) => {
-    const params = new URLSearchParams(currentSearchParams.toString());
-
-    if (value) {
-      params.set("category", value);
-    } else {
-      params.delete("category");
-    }
-    params.set("page", "1");
-    router.push(`${pathname}?${params.toString()}`);
-  };
-
   return (
     <div className="flex w-full flex-wrap items-center justify-center gap-2 ">
       {makeLabelCategoriesData.map(({ label, value }) => (
@@ -52,7 +38,7 @@ const StudyFilterButton = ({
           )}
           {...props}
           key={value || "all"}
-          active={currentSearchParams.get("category") === value}
+          active={searchParams.get("category") === value}
           disabled={false}
           onClick={() => onClickCategory(value || "")}
           color="Main"
--- src/features/study/study-filter/ui/StudyFilterDropdown.tsx
+++ src/features/study/study-filter/ui/StudyFilterDropdown.tsx
@@ -1,44 +1,19 @@
 "use client";
 
+import useStudyFilter from "@/features/study/study-filter/hook/useStudyFilter";
 import { meetTypes, recruitmentTypes } from "@/features/study/study-filter/lib";
 import { StudyFilterDropdownProps } from "@/features/study/study-filter/ui/study-filter.ui.types";
 import Select from "@/shared/components/atoms/Select";
-import { useRouter, usePathname, useSearchParams } from "next/navigation";
 
 const StudyFilterDropdown = ({ type }: StudyFilterDropdownProps) => {
-  const currentSearchParams = useSearchParams();
-  const router = useRouter();
-  const pathname = usePathname();
-
-  const onSelected = (
-    type: StudyFilterDropdownProps["type"],
-    value: string
-  ) => {
-    const params = new URLSearchParams(currentSearchParams.toString());
-
-    if (type === "meet") {
-      if (value) {
-        params.set("meetType", value);
-      } else {
-        params.delete("meetType");
-      }
-    } else {
-      if (value) {
-        params.set("recruitmentStatus", value);
-      } else {
-        params.delete("recruitmentStatus");
-      }
-    }
-    params.set("page", "1");
-    router.push(`${pathname}?${params.toString()}`);
-  };
+  const { searchParams, onClickSelected } = useStudyFilter();
 
   return (
     <Select
       placeholder="진행 방식"
-      defaultValue={currentSearchParams.get("meetType") || ""}
+      defaultValue={searchParams.get("meetType") || ""}
       className="text-mos-gray-500"
-      onChange={(event) => onSelected(type, event.currentTarget.value)}
+      onChange={(event) => onClickSelected(type, event.currentTarget.value)}
     >
       {type === "meet" &&
         meetTypes.map(({ label, value }) => {
