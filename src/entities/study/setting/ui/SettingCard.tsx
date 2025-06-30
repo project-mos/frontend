5a1613042be98a9688390b51c92983cc4a08b4b2
Ian <105128049+minijae011030@users.noreply.github.com>
2025-07-01 02:44:47 +0900

    🚀 [Feat]: 스터디 삭제하기 기능 추가

--- src/entities/study/setting/ui/SettingCard.tsx
+++ src/entities/study/setting/ui/SettingCard.tsx
@@ -1,6 +1,9 @@
 "use client";
 
-import { leaveStudy } from "@/features/studies/services/studies.service";
+import {
+  deleteStudy,
+  leaveStudy,
+} from "@/features/studies/services/studies.service";
 import Button from "@/shared/components/atoms/Button";
 import Card from "@/shared/components/atoms/Card";
 import Typography from "@/shared/components/atoms/Typography";
@@ -27,6 +30,18 @@ const SettingCard = ({ studyId }: { studyId: string }) => {
     }
   };
 
+  const handleDeleteButton = async () => {
+    try {
+      await deleteStudy(studyId);
+      closeModal("delete");
+      router.push(URL.MYPAGE);
+    } catch (e) {
+      console.log("error", e);
+      closeModal("delete");
+      toast.error(`${e}`);
+    }
+  };
+
   return (
     <>
       <Card className="col-span-12 h-fit gap-4 tablet:col-span-9 laptop:col-span-10">
@@ -35,6 +50,17 @@ const SettingCard = ({ studyId }: { studyId: string }) => {
         </Card.Header>
         <Card.Content>
           <div className="flex flex-col gap-2">
+            <div className="flex items-center justify-between rounded-md border border-mos-gray-100 p-5">
+              <div>
+                <Typography.P1 className="font-bold">알림 설정</Typography.P1>
+                <Typography.P3 className="text-[14px] text-mos-gray-700">
+                  해당 스터디의 알림을 설정합니다.
+                </Typography.P3>
+              </div>
+            </div>
+            <Typography.P1 className="mt-3 font-bold text-mos-gray-700">
+              Danger Zone
+            </Typography.P1>
             <div className="flex items-center justify-between rounded-md border border-mos-gray-100 p-5">
               <div>
                 <Typography.P1 className="font-bold">
@@ -54,11 +80,20 @@ const SettingCard = ({ studyId }: { studyId: string }) => {
             </div>
             <div className="flex items-center justify-between rounded-md border border-mos-gray-100 p-5">
               <div>
-                <Typography.P1 className="font-bold">알림 설정</Typography.P1>
+                <Typography.P1 className="font-bold">
+                  스터디 삭제하기
+                </Typography.P1>
                 <Typography.P3 className="text-[14px] text-mos-gray-700">
-                  해당 스터디의 알림을 설정합니다.
+                  해당 스터디를 삭제합니다. 이 설정은 되돌릴 수 없습니다.
                 </Typography.P3>
               </div>
+              <Button.Ghost
+                onClick={() => openModal("delete")}
+                color="Red"
+                active
+              >
+                삭제하기
+              </Button.Ghost>
             </div>
           </div>
         </Card.Content>
@@ -71,6 +106,15 @@ const SettingCard = ({ studyId }: { studyId: string }) => {
         title="탈퇴하기"
         buttonLabel="탈퇴하기"
         onSuccess={handleLeaveButton}
+      />
+      <ActionConfirmModal
+        isOpen={modal.get("delete")!}
+        onClose={() => closeModal("delete")}
+        type="danger"
+        content="정말 해당 스터디를 삭제하시겠습니까?"
+        title="삭제하기"
+        buttonLabel="삭제하기"
+        onSuccess={handleDeleteButton}
       />
     </>
   );
