9c075cd1244e86a17ec1368df24e294b0d09d0ff
yeooonn <rladuswjd0303@naver.com>
2025-07-06 16:32:14 +0900

    🚀 [Feat]: 공지 상단으로 설정 시 데이터 정렬 로직 추가

--- src/features/study/notice/fetch-notice/model/useFetchNotice.ts
+++ src/features/study/notice/fetch-notice/model/useFetchNotice.ts
@@ -9,7 +9,12 @@ const useFetchNotice = (studyId: number) => {
   const { data: noticesData } = useGetNotices(studyId);
   const { data: noticeData } = useGetNotice(studyId, noticeId!);
 
-  return { noticeId, noticesData, noticeData };
+  // pinned true인 항목을 상단에 오도록 정렬
+  const sortedNotices = noticesData?.slice().sort((a, b) => {
+    return (b.pinned === true ? 1 : 0) - (a.pinned === true ? 1 : 0);
+  });
+
+  return { noticeId, noticesData: sortedNotices, noticeData };
 };
 
 export default useFetchNotice;
