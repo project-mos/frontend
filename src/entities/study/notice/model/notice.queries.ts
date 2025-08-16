bf976587e1182c46b2d02e70adecddeef1033afd
yeooonn <rladuswjd0303@naver.com>
2025-08-16 16:41:23 +0900

    🚀 [Feat]: 중요 공지사항 닫기 API 및 type 추가

--- src/entities/study/notice/model/notice.queries.ts
+++ src/entities/study/notice/model/notice.queries.ts
@@ -5,9 +5,10 @@ import {
   postNotice,
   patchNotice,
   deleteNotice,
+  putImportantNotice,
 } from "@/entities/study/notice/api/notice.api";
 import { NoticeRequest } from "@/entities/study/notice/api/notice.api.types";
-import { usePachNoticeProps, UsePostNoticeProps } from "./notice.queries.types";
+import { usePachNoticeProps, UsePostNoticeProps, usePutImportantNoticeProps } from "./notice.queries.types";
 
 // queryKey
 export const noticeKeys = {
@@ -36,7 +37,7 @@ export const useGetNotice = (studyId: number, noticeId: number) =>
 export const usePostNotice = ({
   onSuccess,
   onError,
-  studyId
+  studyId,
 }: UsePostNoticeProps) => {
   const queryClient = useQueryClient();
   return useMutation({
@@ -52,8 +53,12 @@ export const usePostNotice = ({
 };
 
 // 공지사항 수정
-export const usePatchNotice = ({onSuccess,
-  onError, studyId, noticeId}: usePachNoticeProps) => {
+export const usePatchNotice = ({
+  onSuccess,
+  onError,
+  studyId,
+  noticeId,
+}: usePachNoticeProps) => {
   const queryClient = useQueryClient();
   return useMutation({
     mutationFn: (data: NoticeRequest) => patchNotice(studyId, noticeId, data),
@@ -68,14 +73,36 @@ export const usePatchNotice = ({onSuccess,
 };
 
 // 공지사항 삭제
-export const useDeleteNotice = ({onSuccess,
-  onError, studyId, noticeId}: usePachNoticeProps) => {
+export const useDeleteNotice = ({
+  onSuccess,
+  onError,
+  studyId,
+  noticeId,
+}: usePachNoticeProps) => {
   const queryClient = useQueryClient();
   return useMutation({
     mutationFn: () => deleteNotice(studyId, noticeId),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: noticeKeys.all(studyId) });
       onSuccess?.();
+    },
+    onError: (error) => {
+      onError?.(error);
+    },
+  });
+};
+
+// 중요 공지사항 닫기
+export const usePutImportantNotice = ({
+  onError,
+  studyId,
+}: usePutImportantNoticeProps) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: () => putImportantNotice(studyId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: noticeKeys.all(studyId) });
     },
     onError: (error) => {
       onError?.(error);
