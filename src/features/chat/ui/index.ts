e3e4b83d940d8a373902daccdc716ca4f21c16ff
gogleset <choij2494@gmail.com>
2025-08-23 17:27:13 +0900

    🚀 [Feat]: ChatTypeSwitch 컴포넌트 추가 - 개인 및 스터디 채팅 타입 전환 기능 구현

--- src/features/chat/ui/index.ts
+++ src/features/chat/ui/index.ts
@@ -1,8 +1,9 @@
-export { default as ChatErrorState } from './ChatErrorState';
-export { default as ChatBubble } from './ChatBubble';
-export { default as ChatInput } from './ChatInput';
-export { default as ChatItem } from './ChatItem';
-export { default as ChatRoom } from './ChatRoom';
-export { default as ChatTab } from './ChatTab';
-export { default as ChatUserItem } from './ChatUserItem';
-export * from './chat.ui.types';
+export { default as ChatInput } from "./ChatInput";
+export { default as ChatItem } from "./ChatItem";
+export { default as ChatRoom } from "./ChatRoom";
+export { default as ChatTab } from "./ChatTab";
+export { default as ChatBubble } from "./ChatBubble";
+export { default as ChatErrorState } from "./ChatErrorState";
+export { default as ChatUserItem } from "./ChatUserItem";
+export { default as ChatTypeSwitch } from "./ChatTypeSwitch";
+export * from "./chat.ui.types";
